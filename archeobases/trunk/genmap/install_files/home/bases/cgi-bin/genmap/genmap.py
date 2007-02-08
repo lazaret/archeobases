60469edb46300f26ecb37ed0b1c54f634c22e606
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2007-02-08 08:25:19 +0000

    typos

--- archeobases/trunk/genmap/install_files/home/bases/cgi-bin/genmap/genmap.py
+++ archeobases/trunk/genmap/install_files/home/bases/cgi-bin/genmap/genmap.py
@@ -4,6 +4,7 @@
 # version 1.0 Jerome Allet
 # version 1.1 Bertrand lecervoisier -> Passage à l'UTF-8
 
+
 import sys
 import os
 import imp
@@ -38,12 +39,14 @@ else :
 #            Y
 #
 #
-                
+
+
 lesy = []
 for i in range(27) :
     lesy.append(chr(i + ord('A')))
 # IL N'Y A PAS DE V !!!
 del lesy[lesy.index('V')]
+
 
 # ce qui est ci-dessous ne sert que pour etre conservé pour d'autres
 # extractions similaires, pour ne pas avoir à réinventer la roue...
@@ -60,20 +63,21 @@ def get_xy(zone, x, y) :
     newx = float(number) - ((100.0 - float(y)) / 100.0)
     newy = lesy.index(letter) + (float(x) / 100.0)
     return (newx, newy)
-        
-def main(arguments) :            
+
+
+def main(arguments) :
     """Fonction principale"""
     # import du fichier de configuration
     if arguments[1] == "--titre" :
         titre = arguments[2]
         arguments = arguments[2:]
-    else :    
+    else :
         titre = ""
     confname = arguments[1]
-    config = imp.load_source("config", confname)    
-    
+    config = imp.load_source("config", confname)
+
     typetrace = config.TYPETRACE.lower()
-    
+
     # parcours tous les fichiers d'entrée et stocke les données
     # dans des listes.
     urlerreurs = []
@@ -83,7 +87,7 @@ def main(arguments) :
         if fichier == "-" :
             infile = sys.stdin
             fichier = "(stdin)"
-        else :    
+        else :
             infile = open(fichier, "r")
             mustclose = 1
         try :
@@ -92,16 +96,16 @@ def main(arguments) :
                 indicechamps = {}
                 for i in range(len(champs)) :
                     indicechamps[champs[i]] = i
-                izone = indicechamps["zone"]    
-                inumero = indicechamps["numero"]    
-                ibis = indicechamps["bis"]    
+                izone = indicechamps["zone"]
+                inumero = indicechamps["numero"]
+                ibis = indicechamps["bis"]
                 ix = indicechamps["x"]
                 iy = indicechamps["y"]
-                inature = indicechamps["nature"]    
-                iorientation = indicechamps["orientation"]    
-                ilongueur = indicechamps["longueur"]    
-                ilargeur = indicechamps["largeur"]    
-                iepaisseur = indicechamps["epaisseur"]    
+                inature = indicechamps["nature"]
+                iorientation = indicechamps["orientation"]
+                ilongueur = indicechamps["longueur"]
+                ilargeur = indicechamps["largeur"]
+                iepaisseur = indicechamps["epaisseur"]
                 izabsolu = indicechamps["zabsolu"]
                 lines = infile.readlines()
                 for indice in range(len(lines)) :
@@ -109,7 +113,7 @@ def main(arguments) :
                     valeurs = line.strip().split(",")
                     if len(valeurs) != len(champs) :
                         continue # saute la ligne invalide
-                        
+
                     for i in range(len(valeurs)) :
                         v = valeurs[i]
                         if (v.startswith('"') and v.endswith('"')) or \
@@ -117,7 +121,7 @@ def main(arguments) :
                             valeurs[i] = v[1:-1]
                     valeurs[inature] = valeurs[inature].upper()
                     nature = valeurs[inature].strip()
-                    valeurs[izone] = valeurs[izone].upper()        
+                    valeurs[izone] = valeurs[izone].upper()
                     valeurs[iorientation] = valeurs[iorientation].upper() or 'N'
                     urldic = { "action" : "Chercher",
                                "zone" : valeurs[izone],
@@ -129,14 +133,14 @@ def main(arguments) :
                     if not nature :
                         urlerreurs.append(url)
                         erreur = True
-                    else :    
+                    else :
                         try :
                             zabsolu = float(valeurs[izabsolu]) / 100.0
-                            (x, y) = get_xy(valeurs[izone], valeurs[ix], valeurs[iy])        
-                        except (ValueError, TypeError) :    
+                            (x, y) = get_xy(valeurs[izone], valeurs[ix], valeurs[iy])
+                        except (ValueError, TypeError) :
                             urlerreurs.append(url)
                             erreur = True
-                        else :    
+                        else :
                             try :
                                 (longueur, largeur, epaisseur) = (float(valeurs[ilongueur]) / 1000.0, \
                                                                   float(valeurs[ilargeur]) / 1000.0, \
@@ -144,24 +148,24 @@ def main(arguments) :
                                 surface = longueur * largeur
                                 if not surface :
                                     raise ValueError, "Surface nulle."
-                            except (ValueError, TypeError) :    
+                            except (ValueError, TypeError) :
                                 #if (typetrace != "plan") and getattr(config, "INCOMPLETS", True) :
                                 if getattr(config, "INCOMPLETS", True) :
                                     longueur = largeur = epaisseur = config.FORCETAILLE or 0.01
                                     surface = longueur * largeur
-                                else :    
+                                else :
                                     urlerreurs.append(url)
                                     erreur = True
-                            if erreur is False :    
+                            if erreur is False :
                                 naturedatas = donnees.setdefault(nature, [])
                                 naturedatas.append((surface, zabsolu, nature, x, y, longueur, largeur, epaisseur, valeurs[iorientation], url))
-            except (ValueError, KeyError), msg :    
+            except (ValueError, KeyError), msg :
                 sys.stderr.write("Format de fichier invalide : %s\n" % msg)
                 sys.exit(-1)
-        finally :       
+        finally :
             if mustclose :
                 infile.close()
-                
+
     if urlerreurs :
         doc = jaxml.HTML_document()
         doc.html()
@@ -177,8 +181,8 @@ def main(arguments) :
             doc.li()
             doc.a(url, href=url)
             doc._pop()
-        doc._output(config.FICHIERERREURS)    
-        
+        doc._output(config.FICHIERERREURS)
+
     # Maintenant on crée les fichiers de travail
     alldatas = []
     for nature in donnees.keys() :
@@ -213,16 +217,16 @@ def main(arguments) :
                 realorientation = "N"
             else :
                 raise ValueError, "Type de tracé '%s' invalide !" % typetrace
-                
+
             if config.FORCETAILLE :
                 reallength = config.FORCETAILLE
                 realwidth = config.FORCETAILLE
-                
+
             fout.write("%s;%f;%f;%f;%f;%s;%s\n" % (nature, realx, realy, reallength, realwidth, realorientation, url))
-        fout.close()    
-        
-    # si plus d'une nature, on fait la même chose avec l'ensemble des données.     
-    if len(donnees.keys()) > 1 :    
+        fout.close()
+
+    # si plus d'une nature, on fait la même chose avec l'ensemble des données.
+    if len(donnees.keys()) > 1 :
         # on trie par surface décroissante
         alldatas.sort()
         alldatas.reverse()         # permet aux petits objets de s'afficher au dessus des plus gros
@@ -251,60 +255,60 @@ def main(arguments) :
                 realorientation = "N"
             else :
                 raise ValueError, "Type de tracé '%s' invalide !" % typetrace
-                
+
             if config.FORCETAILLE :
                 reallength = config.FORCETAILLE
                 realwidth = config.FORCETAILLE
-                
+
             fout.write("%s;%f;%f;%f;%f;%s;%s\n" % (nature, realx, realy, reallength, realwidth, realorientation, url))
-        fout.close()    
-            
+        fout.close()
+
     # what rasterized map to use as the map background
     if config.IMAGEFOND and os.path.exists(config.IMAGEFOND) :
         bg = maptool.MapBackground(config.IMAGEFOND, \
                                    config.XIMAGE, config.YIMAGE, \
                                    config.LARGEURIMAGE, config.HAUTEURIMAGE, \
                                    config.ROTATIONIMAGE)
-    else :                               
+    else :
         bg = None
-    
+
     # initialize the PDF document
     doc = maptool.PDFMap(author = config.AUTEUR, \
                          title = config.TITRE, \
                          subtitle = config.SOUSTITRE or titre, \
                          papersize = config.PAPIER)
-    
+
     # set the format of the next pages
     for marge in ("gauche", "droite", "haut", "bas") :
         mname = "MARGE%s" % marge.upper()
         if not hasattr(config, mname) :
             setattr(config, mname, getattr(config, "MARGE", "3cm"))
-            
+
     for echelle in ("x", "y") :
         ename = "%sECHELLE" % echelle.upper()
         if not hasattr(config, ename) :
             setattr(config, ename, getattr(config, "ECHELLE", 0.1))
-            
+
     doc.formatpage(config.ORIENTATION, \
                    config.MARGEGAUCHE, \
                    config.MARGEBAS, \
                    config.MARGEDROITE, \
                    config.MARGEHAUT)
-    
+
     # sets the labels for the two axis
     doc.setaxis(xaxis = config.XAXIS, \
         yaxis = config.YAXIS, \
         xaxisalign = config.XALIGNERGRADUATION, \
         yaxisalign = config.YALIGNERGRADUATION)
-                
+
     allfiles = ["%s.data" % nature for nature in donnees.keys()]
     if len(allfiles) > 1 :
         allfiles.append("ALL.data")
         if not config.DETAIL :
             allfiles = allfiles[-1:]
-        
+
     for infile in allfiles :
-        # initialize the new page             
+        # initialize the new page
         doc.initpage(background = bg, \
              xorigin = config.XORIGINE, \
              yorigin = config.YORIGINE, \
@@ -317,84 +321,85 @@ def main(arguments) :
              showygrid = config.YMONTRERGRILLE, \
              showxscale = config.XMONTRERECHELLE, \
              showyscale = config.YMONTRERECHELLE)
-                 
+
         # draws the datas read from he input files
         # according to what's in the configuration file
         doc.draw_datas(config.FICHIERCONFIGNATURES, \
                        infile, \
                        legend = config.LEGENDE)
-            
+
         # ends the current page
         doc.endpage()
-            
+
     # generates the PDF document
     doc.output(config.FICHIERPDF)
-    
-def mainForCGI() :    
+
+
+def mainForCGI() :
     """Main function when used as a CGI script."""
     # first retrieve all variables :
     variables = [ ("auteur", "text", str), ("titre", "text", str), ("soustitre", "text", str),
-                 ("typetrace", "text", str, True), ("papier", "text", str, True), 
+                 ("typetrace", "text", str, True), ("papier", "text", str, True),
                  ("orientation", "text", str, True), ("marge", "text", str, "True"),
                  ("echelle", "text", float, True), ("xmontrerechelle", "bool", bool), ("ymontrerechelle", "bool", bool),
                  ("legende", "text", str), ("xmontrergrille", "bool", bool), ("xalignergraduation", "bool", bool),
                  ("ymontrergrille", "bool", bool), ("yalignergraduation", "bool", bool),
-                 ("xpas", "text", float, True), ("ypas", "text", float, True), ("detail", "bool", bool, True), 
+                 ("xpas", "text", float, True), ("ypas", "text", float, True), ("detail", "bool", bool, True),
                  ("miroir", "bool", bool), ("forcetaille", "text", float),
-                 ("xorigine", "text", float, True), ("yorigine", "text", float, True), 
+                 ("xorigine", "text", float, True), ("yorigine", "text", float, True),
                  ("xaxis", "text", lambda s: s.split(",")), ("yaxis", "text", lambda s: s.split(",")), ("rotation", "text", lambda s: s.strip() or None),
                  ("ximage", "text", float), ("yimage", "text", float), ("largeurimage", "text", float), ("hauteurimage", "text", float),
                  ("rotationimage", "text", lambda s: s.strip() or None), ("imagefond", "file", str), ("url", "text", str, True), ("fichierconfignatures", "file", str, True),
                  ("fichierdonnees", "file", str, True), ("incomplets", "bool", bool)]
-                 
-    form = cgi.FieldStorage()             
+
+    form = cgi.FieldStorage()
     varvalues = {}
     errors = []
-    for variable in variables :        
+    for variable in variables :
         try :
             (varname, vartype, varconv, mandatory) = variable
-        except ValueError :    
+        except ValueError :
             (varname, vartype, varconv, mandatory) = list(variable) + [ False ]
         if form.has_key(varname) :
             if vartype == "bool" :
                 value = form[varname].value
                 if value in [0, 0.0, "0", "OFF", "FALSE", False] :
                     varvalues[varname] = False
-                else :    
+                else :
                     varvalues[varname] = True
-            elif vartype == "text" :      
+            elif vartype == "text" :
                 value = form[varname].value.strip()
                 if mandatory and not value :
                     errors.append("Champ %s vide, or il est obligatoire." % varname)
-                else :    
+                else :
                     try :
                         varvalues[varname] = varconv(value)
-                    except ValueError :    
+                    except ValueError :
                         varvalues[varname] = None
-            elif vartype == "file" :        
+            elif vartype == "file" :
                 value = form[varname].value
                 if mandatory and not value :
                     errors.append("Champ %s vide, or il est obligatoire." % varname)
-                else :    
+                else :
                     varvalues[varname] = value
-            else :        
+            else :
                 errors.append("Type %s non supporté pour le champ %s !" % (vartype, varname))
-        else :                
+        else :
             if mandatory :
                 errors.append("Le champ %s est obligatoire !" % varname)
             elif vartype == "bool" :
                 varvalues[varname] = False
-            elif vartype == "text" :      
+            elif vartype == "text" :
                 if varconv == float :
                     varvalues[varname] = 0.0
-                else :    
+                else :
                     varvalues[varname] = None
-            elif vartype == "file" :      
+            elif vartype == "file" :
                 varvalues[varname] = None
-            else :        
+            else :
                 errors.append("Type %s non supporté pour le champ %s !\n" % (vartype, varname))
-                
-    doc = jaxml.CGI_document()            
+
+    doc = jaxml.CGI_document()
     doc.html()
     doc._push()
     doc.head().title("Résultat")
@@ -408,7 +413,7 @@ def mainForCGI() :
             doc._text(e)
             doc.br()
             doc._pop()
-    else :    
+    else :
         docroot = os.environ.get("DOCUMENT_ROOT", "/var/www")
         referer = os.environ.get("HTTP_REFERER", "")
         (scheme, netloc, subdir, query, fragment) = urlparse.urlsplit(referer)
@@ -426,17 +431,17 @@ def mainForCGI() :
         resultname = os.path.join(directory, "resultat.pdf")
         configpy = open(configpyname, "w")
         configpy.write("# -*- coding: ISO-8859-15 -*-\n# Ne pas modifier la ligne ci-dessus\n\n")
-        for variable in variables :        
+        for variable in variables :
             try :
                 (varname, vartype, varconv, mandatory) = variable
-            except ValueError :    
+            except ValueError :
                 (varname, vartype, varconv, mandatory) = list(variable) + [ False ]
-            if vartype != "file" :    
+            if vartype != "file" :
                 configpy.write("%s = %s\n" % (varname.upper(), repr(varvalues[varname])))
-        for (varname, filename) in [ ("imagefond", "image-fond.jpeg"), 
-                                     ("fichierconfignatures", "natures.conf"), 
+        for (varname, filename) in [ ("imagefond", "image-fond.jpeg"),
+                                     ("fichierconfignatures", "natures.conf"),
                                      ("fichierdonnees", "resultat-requete.datas") ] :
-            value = varvalues[varname]                         
+            value = varvalues[varname]
             if value :
                 outfilename = os.path.join(directory, filename)
                 if varname != "fichierdonnees" :
@@ -444,55 +449,55 @@ def mainForCGI() :
                 outfile = open(outfilename, "w+b")
                 outfile.write(varvalues[varname])
                 outfile.close()
-            else :   
+            else :
                 configpy.write('%s = ""\n' % varname.upper())
-                
+
         configpy.write('FICHIERERREURS = "%s"\n' % erreursname)
         configpy.write('FICHIERPDF = "%s"\n' % resultname)
-        configpy.close()        
-        
+        configpy.close()
+
         donneesoriginales = os.path.join(directory, "resultat-requete.datas")
         main(("genmap", configpyname, donneesoriginales))
-        
+
         doc.h3("Résultat :")
         doc.ul()
-        
+
         doc._push()
         doc.li()
         doc._text("Fichier PDF :")
         doc.a("%s/resultat.pdf" % url, href="%s/resultat.pdf" % url, target="_new")
         doc._pop()
-        
+
         doc._push()
         doc.li()
         doc._text("Erreurs :")
         if os.path.exists(erreursname) :
             doc.a("%s/erreurs.html" % url, href="%s/erreurs.html" % url, target="_new")
-        else :    
+        else :
             doc.strong("Aucune")
         doc._pop()
-        
+
         doc._push()
         doc.li()
         doc._text("Fichier de configuration :")
         doc.a("%s/configuration" % url, href="%s/configuration" % url, target="_new")
         doc._pop()
-        
+
         doc._push()
         doc.li()
         doc._text("Fichier de configuration des natures :")
         doc.a("%s/natures.conf" % url, href="%s/natures.conf" % url, target="_new")
         doc._pop()
-        
+
         doc._push()
         doc.li()
         doc._text("Données originales :")
         doc.a("%s/resultat-requete.datas" % url, href="%s/resultat-requete.datas" % url, target="_new")
         doc._pop()
-        
+
     doc._output("-")
-                
-        
+
+
 if __name__ == '__main__' :
     if len(sys.argv) == 1 :
         if os.environ.has_key("REQUEST_METHOD") :

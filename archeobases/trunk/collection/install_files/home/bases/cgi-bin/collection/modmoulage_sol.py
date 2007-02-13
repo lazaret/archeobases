8e3e35118d4b0317e4ebda89651d5d9e057f9d48
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2007-02-13 09:41:23 +0000

    New updated top-comment statment

--- archeobases/trunk/collection/install_files/home/bases/cgi-bin/collection/modmoulage_sol.py
+++ archeobases/trunk/collection/install_files/home/bases/cgi-bin/collection/modmoulage_sol.py
@@ -1,8 +1,8 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 #
 # Collection - (c) 2000-2007 LDLP (Laboratoire Départemental de Prehistoire du Lazaret)
-# http://lazaret.unice.fr/opensource/
+# http://lazaret.unice.fr/opensource/ - opensource@lazaret.unice.fr
 #
 # You're welcome to redistribute this software under the
 # terms of the GNU General Public Licence version 2
@@ -11,6 +11,7 @@
 # which should come along with this software, or visit
 # the Free Software Foundation's WEB site http://www.fsf.org
 #
+
 
 import collectionconf
 import moulage_sol
--- archeobases/trunk/collection/install_files/home/bases/cgi-bin/collection/recup-image.py
+++ archeobases/trunk/collection/install_files/home/bases/cgi-bin/collection/recup-image.py
@@ -1,8 +1,8 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 #
 # Collection - (c) 2000-2007 LDLP (Laboratoire Départemental de Prehistoire du Lazaret)
-# http://lazaret.unice.fr/opensource/
+# http://lazaret.unice.fr/opensource/ - opensource@lazaret.unice.fr
 #
 # You're welcome to redistribute this software under the
 # terms of the GNU General Public Licence version 2
@@ -14,6 +14,7 @@
 
 # 2003 Rachel Vaudron
 # recupere les images d'un cd pour les insérer dans la base
+
 
 import sys
 import os

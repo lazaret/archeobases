f5f772065193b1e66e5ed33707ce71c3263aeffd
Bertrand Lecervoisier <miniwark@miniwark.com>
2007-05-02 11:56:33 +0000

    Commemts translated to english

--- archeobases/trunk/sauve_bases/setup.py
+++ archeobases/trunk/sauve_bases/setup.py
@@ -1,27 +1,30 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-"""  Script d'installation du module sauve_bases.py
+"""  Install Script for sauve_bases module
 """
 
 
-#bootstrap pour installer setuptools si besoin
+# automatic install of setuptools id necessary
 from ez_setup import use_setuptools
 use_setuptools()
 
-#installation propement dite à l'aide de setuptools
+# Begin of the install script using setuptools
 from setuptools import setup
+import sauve_bases # import module for version number use
 
-setup(name= "sauve_bases",
-    version= "0.5",
-    description= "Module et script de sauvegarde de bases donnees",
+setup(
+    # Setuptools-Distutils Meta-datas
+    name= "sauve_bases",
+    version= sauve_bases.__version__,
+    description= "Module and scripts for database regular saving",
     author= "Bertrand Lecervoisier",
     author_email= "opensource@lazaret.unice.fr",
     url= "http://lazaret.unice.fr/opensource/",
     license= "GNU GPL v2",
-
+    # Distributed module and scripts
     py_modules = ['sauve_bases'],
     scripts=['sauvedb_jour.sh', 'sauvedb_mois.sh', 'sauvedb_semaine.sh'],
-
+    # Prerequistes
     install_requires = ['PyGreSQL>=3.0'],
     )
 

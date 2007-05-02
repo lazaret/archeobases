59920e1663609f8b3945c92c354e3e2ae7d33d9f
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2007-05-02 08:17:38 +0000

    Add the bootstrap to install setuptools if necessary

--- archeobases/trunk/sauve_bases/setup.py
+++ archeobases/trunk/sauve_bases/setup.py
@@ -3,20 +3,25 @@
 """  Script d'installation du module sauve_bases.py
 """
 
-#from distutils.core import setup
+
+#bootstrap pour installer setuptools si besoin
+from ez_setup import use_setuptools
+use_setuptools()
+
+#installation propement dite à l'aide de setuptools
 from setuptools import setup
 
-
 setup(name= "sauve_bases",
-      version= "0.5",
-      description= "Module et script de sauvegarde de bases donnees",
-      author= "Bertrand Lecervoisier",
-      author_email= "opensource@lazaret.unice.fr",
-      url= "http://lazaret.unice.fr/opensource/",
-      license= "GNU GPL v2",
-      
-      py_modules = ['sauve_bases'],
-      scripts=['sauvedb_jour.sh', 'sauvedb_mois.sh', 'sauvedb_semaine.sh'],
-      
-      install_requires = ['PyGreSQL>=0.1'],
-      )
+    version= "0.5",
+    description= "Module et script de sauvegarde de bases donnees",
+    author= "Bertrand Lecervoisier",
+    author_email= "opensource@lazaret.unice.fr",
+    url= "http://lazaret.unice.fr/opensource/",
+    license= "GNU GPL v2",
+
+    py_modules = ['sauve_bases'],
+    scripts=['sauvedb_jour.sh', 'sauvedb_mois.sh', 'sauvedb_semaine.sh'],
+
+    install_requires = ['PyGreSQL>=0.1'],
+    )
+

ecfd3f00b62aa43ad8a436481198ae18e2e67a1b
Bertrand Lecervoisier <miniwark@miniwark.com>
2007-05-09 13:23:45 +0000

    add distutils meta-datas

--- archeobases/trunk/savetape/setup.py
+++ archeobases/trunk/savetape/setup.py
@@ -16,15 +16,17 @@ from savetape import __author__
 
 setup(
     # Setuptools-Distutils Meta-datas
-    name= "savetape",
-    version= __version__,
-    description= "Module and script for savinf directories on unix tape",
-    author= __author__,
-    author_email= "opensource@lazaret.unice.fr",
-    url= "http://lazaret.unice.fr/opensource/",
-    license= "GNU GPL v2",
+    name = "savetape",
+    version = __version__,
+    description = "Module and script for saving directories on unix tape",
+    long_description = "Savetape is a Python script and a module used to regulary save with cron specified folders on unix tape",
+    author = __author__,
+    author_email = "opensource@lazaret.unice.fr",
+    url = "http://lazaret.unice.fr/opensource/",
+    license = "GNU GPL v2",
+    platforms = "UNIX",
     # Distributed module and scripts
     py_modules = ['savetape'],
-    scripts=['savetotape.sh']
+    scripts =['savetotape.sh']
     )
 

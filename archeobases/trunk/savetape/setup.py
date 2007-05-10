52d6c683515311624139b6a7b947ae5df2d63914
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2007-05-10 14:12:12 +0000

    - Add target system testing and warning on non-linux system - Update the distutils meta-datas

--- archeobases/trunk/savetape/setup.py
+++ archeobases/trunk/savetape/setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-"""  Install Script for savetape module based on setuptools
+"""  Install script for savetape package based on Distutils/Setuptools
 """
 
 
@@ -8,25 +8,52 @@
 from ez_setup import use_setuptools
 use_setuptools()
 
+
 # Begin of the install script using setuptools
+import sys
+import string
 from setuptools import setup
-from savetape import __version__
-from savetape import __author__
+import savetape
+
+
+# Ask to confirm the installation on other platforms than linux v2
+if sys.platform != "linux2" and "install" in sys.argv:
+    print "This software have only be tested on Linux2 platform."
+    print "Your detected platform is : "+sys.platform
+    install = raw_input("If you whant to install this software anyway please type 'YES' : ")
+    if string.upper(install)!= "YES":
+        raise SystemExit("Exiting install...")
 
 
 setup(
-    # Setuptools-Distutils Meta-datas
+    # Distutils Meta-datas
     name = "savetape",
-    version = __version__,
+    version = savetape.__version__,
     description = "Module and script for saving directories on unix tape",
     long_description = "Savetape is a Python script and a module used to regulary save with cron specified folders on unix tape",
-    author = __author__,
+    author = savetape.__author__,
     author_email = "opensource@lazaret.unice.fr",
-    url = "http://lazaret.unice.fr/opensource/",
-    license = "GNU GPL v2",
-    platforms = "UNIX",
-    # Distributed module and scripts
-    py_modules = ['savetape'],
-    scripts =['savetotape.sh']
+    url = savetape.__url__,
+    license = savetape.__license__,
+    platforms = "Unix",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: Unix",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: System :: Archiving :: Backup",
+    ],
+
+    # Install & test options
+    #test_suite = "",
+    zip_safe = True,
+
+    # Distributed files (module, scripts & data files)
+    py_modules = ["savetape"],
+    scripts =["savetotape.sh"],
+    data_files =[("/etc",["savetape.conf"])]
     )
 

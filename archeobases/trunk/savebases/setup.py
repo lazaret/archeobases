4ccdd368d8b55b68a0f0e421f6dfc2c575c3676f
Bertrand Lecervoisier <miniwark@miniwark.com>
2007-05-11 08:45:58 +0000

    - update the setup.py Meta-datas - remove setuptools packaging. We prefer distutils

--- archeobases/trunk/savebases/setup.py
+++ archeobases/trunk/savebases/setup.py
@@ -1,34 +1,49 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-"""  Install Script for savebases module based on setuptools
+"""  Install script for savebases package based on Distutils
 """
 
 
-# automatic install of setuptools id necessary
-from ez_setup import use_setuptools
-use_setuptools()
+import sys
+from distutils.core import setup
+import savebases as package
 
-# Begin of the install script using setuptools
-from setuptools import setup
-from savebases import __version__
-from savebases import __author__
+
+# Ask to install first PyGreSQL version 3 if necessary
+if "install" in sys.argv:
+    print "test"
+    #print "This software have only be tested on Linux2 platform."
+    #print "Your detected platform is : "+sys.platform
+    #install = raw_input("If you whant to install this software anyway please type 'YES' : ")
+    #if string.upper(install)!= "YES":
+    #    raise SystemExit("Exiting install...")
 
 
 setup(
     # Setuptools-Distutils Meta-datas
-    name = "savebases",
-    version = __version__,
+    name = package.__name__,
+    version = package.__version__,
     description = "Module and scripts for database regular saving",
     long_description = "Savetape is a set Python scripts and a module used to regulary save with cron specified databases into folders",
-    author = __author__,
-    author_email = "opensource@lazaret.unice.fr",
-    url = "http://lazaret.unice.fr/opensource/",
-    license = "GNU GPL v2",
-    platforms = "UNIX",
-    # Prerequistes
-    install_requires = ['PyGreSQL>=3.0'],
+    author = package.__author__,
+    author_email = package.__author_email__,
+    url = package.__url__,
+    license = package.__license__,
+    platforms = "Unix",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: Unix",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: Database",
+        ],
     # Distributed module and scripts
     py_modules = ['savebases'],
-    scripts =['savedb_day.sh', 'savedb_week.sh', 'savedb_month.sh']
+    scripts =['savedb_day.sh', 'savedb_week.sh', 'savedb_month.sh'],
+    data_files =[("/etc",["savebases.conf"])]
     )
 

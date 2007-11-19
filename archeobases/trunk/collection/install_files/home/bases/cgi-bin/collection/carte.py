38493805ed414423363f2bc46577fb05d3f74a20
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2007-11-19 21:30:43 +0000

    Minor typo correction

--- archeobases/trunk/collection/install_files/home/bases/cgi-bin/collection/carte.py
+++ archeobases/trunk/collection/install_files/home/bases/cgi-bin/collection/carte.py
@@ -156,7 +156,7 @@ class Carte(collectiondata.Data) :
                         self.__doc__.push()
                         afficheclefs.champ_saisie(self,"m_date_modif", "Modif le", 10, 20, " ", enreg, penreg)
                         self.__doc__.pop()
-                self.__doc__.pop()
+                #self.__doc__.pop()
 
         def m_saisie_base_to_form(self, enreg, penreg = None) :
                 if enreg != None:

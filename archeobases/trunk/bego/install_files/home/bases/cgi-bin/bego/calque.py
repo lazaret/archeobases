954f8108ca59b2304a783224546ca95b4fd9329e
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2008-06-23 07:09:46 +0000

    Code cleaning to a more PEP8 one, see ticket #81

--- archeobases/trunk/bego/install_files/home/bases/cgi-bin/bego/calque.py
+++ archeobases/trunk/bego/install_files/home/bases/cgi-bin/bego/calque.py
@@ -12,7 +12,6 @@
 # the Free Software Foundation's WEB site http://www.fsf.org
 #
 
-
 import sys
 import os
 import string
@@ -20,133 +19,118 @@ import database
 import begoconf
 
 
-def transfere_fichier(infile, taille, mogrify) :
-        """Stocke le fichier sur le disque et lance la commande mogrify appropriee"""
-        inf = open(infile, "rb")
-        fout = open(taille, "wb")
-        fout.write(inf.read())
-        fout.close()
-        inf.close()
-        cmd = mogrify + ' "' + taille + '" >/dev/null'
-        if os.system(cmd) :
-		raise "Erreur"
+def transfere_fichier(infile, taille, mogrify):
+    """Stocke le fichier sur le disque et lance la commande mogrify appropriee"""
+    inf = open(infile, "rb")
+    fout = open(taille, "wb")
+    fout.write(inf.read())
+    fout.close()
+    inf.close()
+    cmd = mogrify + ' "' + taille + '" >/dev/null'
+    if os.system(cmd) :
+        raise "Erreur"
 
-def creer_image(z = "", g = "", r = "", f = "", fg = "") :
-        # on commence par créer le repertoire destination s'il n'existe pas déjà
-        (zone, groupe, roche, face, figure) = (z, g, r, f, fg)
-        try :
-                z = "Z" + z
-                rep = begoconf.image_fullname(z)
-                if not os.path.isdir(rep) :
-                        os.mkdir(rep, 0755)
+def creer_image(z="", g="", r="", f="", fg=""):
+    # on commence par créer le repertoire destination s'il n'existe pas déjà
+    (zone, groupe, roche, face, figure) = (z, g, r, f, fg)
+    try:
+        z = "Z" + z
+        rep = begoconf.image_fullname(z)
+        if not os.path.isdir(rep):
+            os.mkdir(rep, 0755)
+        if g:
+            g = "G" + g
+            rep = begoconf.image_fullname(z + os.sep + g)
+            if not os.path.isdir(rep):
+                os.mkdir(rep, 0755)
+        if r:
+            r = "R" + r
+            rep = begoconf.image_fullname(z + os.sep + g + os.sep + r)
+            if not os.path.isdir(rep):
+                os.mkdir(rep, 0755)
+        if f:
+            f = "F" + f
+            rep = begoconf.image_fullname(z + os.sep + g + os.sep + r + os.sep + f)
+            if not os.path.isdir(rep):
+                os.mkdir(rep, 0755)
+        if fg:
+            fg = "Fg" + fg
+            rep = begoconf.image_fullname(z + os.sep + g + os.sep + r + os.sep + f + os.sep + fg)
+            if not os.path.isdir(rep):
+                os.mkdir(rep, 0755)
+    except OSError, msg:
+        sys.stderr.write("Impossible de créer le repertoire [%s] ou l'un de ses composants: %s" % (rep, msg))
+    #
+    idphoto = db.query("select nextval('seq_photofigure');").dictresult()[0]['nextval']
+    fname = rep + os.sep + `idphoto`
+    small = fname + "s.jpeg"
+    big = fname + ".jpeg"
+    normale = fname + ".tiff"
+    # on insère maintenant la photo dans la base
+    # mais seulement si le chargement des images a fonctionné
+    try:
+        transfere_fichier("tempo.tiff", normale, begoconf.mogrify_normale)
+        transfere_fichier("tempo.tiff", small, begoconf.mogrify_small)
+        transfere_fichier("tempo.tiff", big, begoconf.mogrify_Moyenne)
+        req = "INSERT INTO photofigure(idphoto, zone, groupe, roche, face, figure) VALUES (" + db.quote(idphoto, "decimal") + ", " + db.quote(zone, "decimal") + ", " + db.quote(groupe, "decimal") + ", " + db.quote(roche, "text") + ", " + db.quote(face, "text") + ", " + db.quote(figure, "text") + ");"
+        db.query(req)
+        print idphoto
+    sys.stdout.flush()
+    except:
+        sys.stderr.write("Impossible de transfèrer la photo [%s]\n"  % (fname))
+    sys.stderr.flush()
 
-                if g :
-                        g = "G" + g
-                        rep = begoconf.image_fullname(z + os.sep + g)
-                        if not os.path.isdir(rep) :
-                                os.mkdir(rep, 0755)
-
-                if r :
-                        r = "R" + r
-                        rep = begoconf.image_fullname(z + os.sep + g + os.sep + r)
-                        if not os.path.isdir(rep) :
-                                os.mkdir(rep, 0755)
-
-                if f :
-                        f = "F" + f
-                        rep = begoconf.image_fullname(z + os.sep + g + os.sep + r + os.sep + f)
-                        if not os.path.isdir(rep) :
-                                os.mkdir(rep, 0755)
-
-                if fg :
-                        fg = "Fg" + fg
-                        rep = begoconf.image_fullname(z + os.sep + g + os.sep + r + os.sep + f + os.sep + fg)
-                        if not os.path.isdir(rep) :
-                                os.mkdir(rep, 0755)
-        except OSError, msg:
-                sys.stderr.write("Impossible de créer le repertoire [%s] ou l'un de ses composants: %s" % (rep, msg))
-
-        #
-        idphoto = db.query("select nextval('seq_photofigure');").dictresult()[0]['nextval']
-        fname = rep + os.sep + `idphoto`
-        small = fname + "s.jpeg"
-        big = fname + ".jpeg"
-        normale = fname + ".tiff"
-
-        # on insère maintenant la photo dans la base
-        # mais seulement si le chargement des images a fonctionné
-        try :
-                transfere_fichier("tempo.tiff", normale, begoconf.mogrify_normale)
-                transfere_fichier("tempo.tiff", small, begoconf.mogrify_small)
-                transfere_fichier("tempo.tiff", big, begoconf.mogrify_Moyenne)
-
-                req = "INSERT INTO photofigure(idphoto, zone, groupe, roche, face, figure) VALUES (" + db.quote(idphoto, "decimal") + ", " + db.quote(zone, "decimal") + ", " + db.quote(groupe, "decimal") + ", " + db.quote(roche, "text") + ", " + db.quote(face, "text") + ", " + db.quote(figure, "text") + ");"
-                db.query(req)
-                print idphoto
-		sys.stdout.flush()
-        except :
-                sys.stderr.write("Impossible de transfèrer la photo [%s]\n"  % (fname))
-		sys.stderr.flush()
-
-def parcours(param, dirname, names) :
+def parcours(param, dirname, names):
+    ######## TODO indentation bugee a verifier
         (db, sf) = param
-        arabe = {"I":"1","II":"2","III":"3","IV":"4","V":"5","VI":"6","VII":"7","VIII":"8","IX":"9","X":"10","XI":"11","XII":"12","XIII":"13","XIV":"14","XV":"15","XVI":"16","XVII":"17","XVIII":"18","XIX":"19","XX":"20","XXI":"21"}
-        grec = {"a":"@a","b":"@b","c":"@c","d":"@d","e":"@e","f":"@f","g":"@g","h":"@h","i":"@i","j":"@j","k":"@k","l":"@l","m":"@m","n":"@n","o":"@o","p":"@p","q":"@q","r":"@r","s":"@s","t":"@t","u":"@u","v":"@v","w":"@w","x":"@x","y":"@y","z":"@z"}
+        arabe = {"I": "1", "II": "2", "III": "3", "IV": "4", "V": "5", "VI": "6", "VII": "7", "VIII": "8", "IX": "9",
+            "X": "10", "XI": "11", "XII": "12", "XIII": "13", "XIV": "14", "XV": "15", "XVI": "16", "XVII": "17",
+            "XVIII": "18", "XIX": "19", "XX": "20", "XXI": "21"}
+        grec = {"a": "@a", "b": "@b", "c": "@c", "d": "@d", "e": "@e", "f": "@f", "g": "@g", "h": "@h", "i": "@i",
+            "j": "@j", "k":"@k", "l": "@l", "m": "@m", "n": "@n", "o": "@o", "p": "@p", "q": "@q", "r": "@r", "s": "@s",
+            "t": "@t", "u":"@u", "v":"@v", "w": "@w", "x": "@x", "y": "@y", "z": "@z"}
+    sf.write("")
+        for filename in names:
+            fichier = os.path.join(dirname, filename)
+            if os.path.isfile(fichier):
+                filename = string.replace(filename,'Conti','')
+                # on decoupe le nom du fichier en z,g,r,f,fg
+                if string.find(fichier, 'Gravure') >= 0 :
+            sf.write("[%s]\n" % (filename))
+                z = string.find(filename,'Z')
+                g = string.find(filename,'G')
+                r = string.find(filename,'R')
+                t1 = string.find(filename,'-')
+                f = string.find(filename,'F')
+                t2 = string.rfind(filename,'-')
+                pt = string.find(filename,'.')
+                # on convertit les chiffres romain en chiffres arabes
+                zone = arabe[filename[z+1:g]]
+                groupe = arabe[filename[g+1:r]]
+                roche  = filename[r+1:t1]
+                # on remplace les petites lettres par @petite lettre
+                for i in grec.keys():
+                    if string.find(roche, 'bis') < 0:
+                        roche = string.replace(roche,i,grec[i])
+                if t2 != t1:
+                    figure = filename[t1+1:t2]
+                else:
+                    figure = filename[t1+1:pt]
+                # on recupere la face correspondante à la figure
+                requete = "SELECT face FROM figure WHERE zone=%s AND groupe=%s AND roche=%s AND figure=%s;" % (db.quote(zone,"decimal"), db.quote(groupe,"decimal"), db.quote(roche,"text"), db.quote(figure,"text"))
+                resultat = db.query(requete).dictresult()
+                if len(resultat):
+                    face = resultat[0]["face"]
+                    #on insere la nouvelle photo dans la table photofigure
+                    if not os.system('zcat <"%s" >%s' % (fichier, "tempo.tiff")):
+                        creer_image(zone, groupe, roche, face, figure)
+                    else:
+                        sys.stderr.write("erreur sur [%s]\n" % fichier)
+                else:
+                    sf.write("[%s] => [%s %s %s %s]\n" % (fichier, zone, groupe, roche, figure))
 
 
-	sf.write("")
-
-
-        for filename in names :
-                fichier = os.path.join(dirname, filename)
-                if os.path.isfile(fichier) :
-
-                        filename = string.replace(filename,'Conti','')
-
-                        # on decoupe le nom du fichier en z,g,r,f,fg
-                        if string.find(fichier, 'Gravure') >= 0 :
-				sf.write("[%s]\n" % (filename))
-
-                                z = string.find(filename,'Z')
-                                g = string.find(filename,'G')
-                                r = string.find(filename,'R')
-                                t1 = string.find(filename,'-')
-                                f = string.find(filename,'F')
-                                t2 = string.rfind(filename,'-')
-                                pt = string.find(filename,'.')
-
-                                # on convertit les chiffres romain en chiffres arabes
-                                zone = arabe[filename[z+1:g]]
-                                groupe = arabe[filename[g+1:r]]
-                                roche  = filename[r+1:t1]
-
-                                # on remplace les petites lettres par @petite lettre
-                                for i in grec.keys():
-                                        if string.find(roche, 'bis') < 0:
-                                                roche = string.replace(roche,i,grec[i])
-
-                                if t2 != t1:
-                                        figure = filename[t1+1:t2]
-                                else:
-                                        figure = filename[t1+1:pt]
-
-                                # on recupere la face correspondante à la figure
-                                requete = "SELECT face FROM figure WHERE zone=%s AND groupe=%s AND roche=%s AND figure=%s;" % (db.quote(zone,"decimal"), db.quote(groupe,"decimal"), db.quote(roche,"text"), db.quote(figure,"text"))
-                                resultat = db.query(requete).dictresult()
-                                if len(resultat) :
-                                        face = resultat[0]["face"]
-                                        #on insere la nouvelle photo dans la table photofigure
-                                        if not os.system('zcat <"%s" >%s' % (fichier, "tempo.tiff")) :
-                                                creer_image(zone, groupe, roche, face, figure)
-                                        else :
-                                                sys.stderr.write("erreur sur [%s]\n" % fichier)
-                                else :
-                                        sf.write("[%s] => [%s %s %s %s]\n" % (fichier, zone, groupe, roche, figure))
-
-
-
-db = database.DataBase(database = "bego", username = "bego", debuglevel = 0)
-
+db = database.DataBase(database="bego", username="bego", debuglevel=0)
 
 sf = open("sansface.lst", "w")
 sf.write("Liste des fichiers n'ayant pas de face correspondante\n")
--- archeobases/trunk/bego/install_files/home/bases/cgi-bin/bego/conversion.py
+++ archeobases/trunk/bego/install_files/home/bases/cgi-bin/bego/conversion.py
@@ -12,55 +12,52 @@
 # the Free Software Foundation's WEB site http://www.fsf.org
 #
 
-
 import sys
 import os
 
-
+### BUG guillemets non fermes
 def conversion(romain):
-	if len(romain) >= 1:
-		if romain[1] == 'I':
-			arabe = 1
-		else:
-			if romain[1] == 'V':
-				arabe = 5
-			if romain[1] == 'X':
-				arabe = 10
-	if len(romain) >= 2:
-		if romain[2] == 'I':
-			arabe = arabe + 1
-		else:
-			if romain[1] == 'V':
-				arabe = 4
-			if romain[1] == 'X':
-				arabe = 9
+    """ Convertit du chiffre romain en chiffre arabe."""
+    if len(romain) >= 1:
+        if romain[1] == 'I':
+            arabe = 1
+        else:
+            if romain[1] == 'V':
+                arabe = 5
+            if romain[1] == 'X':
+                arabe = 10
+    if len(romain) >= 2:
+        if romain[2] == 'I':
+            arabe = arabe + 1
+        else:
+            if romain[1] == 'V':
+                arabe = 4
+            if romain[1] == 'X':
+                arabe = 9
+    if len(romain) == 3:
+        arabe = arabe + 1
+    return arabe
 
-	if len(romain) == 3:
-		arabe = arabe + 1
+def parcours(arg, dirname, names):
+    for filename in names:
+        fichier = os.path.join(dirname, filename)
+        if os.path.isfile(fichier):
+            for car in filename:
+                if (filename[car == 'Z'):
+                    while filename[car] != 'G':
+                        romain = romain + filename[car]
+                        zone   = conversion(romain)
+                        print "zone:",zone
 
-	return arabe
-
-def parcours(arg, dirname, names) :
-	for filename in names :
-		fichier = os.path.join(dirname, filename)
-		if os.path.isfile(fichier) :
-
-			for car in filename:
-				if (filename[car == 'Z'):
-			 		while filename[car] != 'G':
-						romain = romain + filename[car]
-						zone   = conversion(romain)
-						print "zone:",zone
-
-			# ici tu dois decomposer filename
-			# (pas fichier pour ne pas avoir le nom de rep)
-			# pour en extraire Z, G, R, F, f
-			# et faire ce qui t'interesse
-			# et taiter cette fois-ci fichier avec mogrify
-			print filename,"est un fichier"
-		else :
-			# ici normalement tu ne fais rien
-			print fichier,"est un repertoire"
+            # ici tu dois decomposer filename
+            # (pas fichier pour ne pas avoir le nom de rep)
+            # pour en extraire Z, G, R, F, f
+            # et faire ce qui t'interesse
+            # et taiter cette fois-ci fichier avec mogrify
+            print filename,"est un fichier"
+        else:
+            # ici normalement tu ne fais rien
+            print fichier,"est un repertoire"
 
 # le None ici est en fait le arg de parcours,
 # mais on n'en a pas besoin

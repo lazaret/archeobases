043ef64f5931ac2cbf34fd342f09f7f55bb05c78
Bertrand Lecervoisier <miniwark@miniwark.com>
2008-07-07 17:03:35 +0000

    Code cleaning to a more PEP8 one, see ticket #81

--- archeobases/trunk/bego/utilitaires/recup-image.py
+++ archeobases/trunk/bego/utilitaires/recup-image.py
@@ -20,85 +20,77 @@ import string
 import database
 import archeoconf
 
+
 def transfere_fichier(infile, taille, mogrify) :
-        """Stocke le fichier sur le disque et lance la commande mogrify appropriee"""
-        inf = open(infile, "rb")
-        fout = open(taille, "wb")
-        fout.write(inf.read())
-        fout.close()
-        inf.close()
-        cmd = mogrify + ' "' + taille + '" >/dev/null'
-        if os.system(cmd) :
-		raise "Erreur"
+    """Stocke le fichier sur le disque et lance la commande mogrify appropriee"""
+    inf = open(infile, "rb")
+    fout = open(taille, "wb")
+    fout.write(inf.read())
+    fout.close()
+    inf.close()
+    cmd = mogrify + ' "' + taille + '" >/dev/null'
+    if os.system(cmd) :
+        raise "Erreur"
 
 def creer_image(z = "", n = "", b = "") :
-        # on commence par creer le repertoire destination s'il n'existe pas deja
-        (zone, numero, bis, face) = (z, n, b)
-        try :
-                z = "Z" + z
-                rep = archeoconf.image_fullname(z)
-                if not os.path.isdir(rep) :
-                        os.mkdir(rep, 0755)
+    # on commence par creer le repertoire destination s'il n'existe pas deja
+    (zone, numero, bis, face) = (z, n, b)
+    try :
+        z = "Z" + z
+        rep = archeoconf.image_fullname(z)
+        if not os.path.isdir(rep) :
+                os.mkdir(rep, 0755)
+        if n :
+            n = "N" + n
+            rep = archeooconf.image_fullname(z + os.sep + n)
+            if not os.path.isdir(rep) :
+                os.mkdir(rep, 0755)
+        if b :
+            b = "B" + b
+            rep = archeooconf.image_fullname(z + os.sep + n + os.sep + b)
+            if not os.path.isdir(rep) :
+                os.mkdir(rep, 0755)
+    except OSError, msg:
+        sys.stderr.write("Impossible de créer le répertoire [%s] ou l'un de ses composants: %s" % (rep, msg))
+    #
+    idphoto = db.query("select nextval('seq_photoindustrie');").dictresult()[0]['nextval']
+    fname = rep + os.sep + `idphoto`
+    small = fname + "s.jpeg"
+    big = fname + ".jpeg"
+    normale = fname + ".tiff"
+    # on insere maintenant la photo dans la base
+    # mais seulement si le chargement des images a fonctionne
+    try :
+        transfere_fichier("tempo.tiff", normale, archeoconf.mogrify_normale)
+        transfere_fichier("tempo.tiff", small, archeoconf.mogrify_small)
+        transfere_fichier("tempo.tiff", big, archeoconf.mogrify_Moyenne)
 
-                if n :
-                        n = "N" + n
-                        rep = archeooconf.image_fullname(z + os.sep + n)
-                        if not os.path.isdir(rep) :
-                                os.mkdir(rep, 0755)
+        req = "INSERT INTO photoindustrie(idphoto, zone, numero, bis) VALUES (" + db.quote(idphoto, "decimal") + ", " + db.quote(zone, "text") + ", " + db.quote(numero, "decimal") + ", " + db.quote(bis, "text") + ");"
+        db.query(req)
+        print idphoto
+    sys.stdout.flush()
+    except :
+        sys.stderr.write("Impossible de transf�rer la photo [%s]\n"  % (fname))
+    sys.stderr.flush()
 
-                if b :
-                        b = "B" + b 
-                        rep = archeooconf.image_fullname(z + os.sep + n + os.sep + b)
-                        if not os.path.isdir(rep) :
-                                os.mkdir(rep, 0755)
-        except OSError, msg:
-                sys.stderr.write("Impossible de créer le répertoire [%s] ou l'un de ses composants: %s" % (rep, msg))
-
-        #
-        idphoto = db.query("select nextval('seq_photoindustrie');").dictresult()[0]['nextval']
-        fname = rep + os.sep + `idphoto`
-        small = fname + "s.jpeg"
-        big = fname + ".jpeg"
-        normale = fname + ".tiff"
-
-        # on insere maintenant la photo dans la base
-        # mais seulement si le chargement des images a fonctionne
-        try :
-                transfere_fichier("tempo.tiff", normale, archeoconf.mogrify_normale)
-                transfere_fichier("tempo.tiff", small, archeoconf.mogrify_small)
-                transfere_fichier("tempo.tiff", big, archeoconf.mogrify_Moyenne)
-                
-                req = "INSERT INTO photoindustrie(idphoto, zone, numero, bis) VALUES (" + db.quote(idphoto, "decimal") + ", " + db.quote(zone, "text") + ", " + db.quote(numero, "decimal") + ", " + db.quote(bis, "text") + ");"
-                db.query(req)
-                print idphoto
-		sys.stdout.flush()
-        except :
-                sys.stderr.write("Impossible de transf�rer la photo [%s]\n"  % (fname))
-		sys.stderr.flush()
-                        
 def parcours(param, dirname, names) :
-        (db, sf) = param
-	sf.write("") 
-
+    (db, sf) = param
+    sf.write("")
         for filename in names :
-                fichier = os.path.join(dirname, filename)
-                if os.path.isfile(fichier) :
-                        
-                        # on decoupe le nom du fichier en z,n,b
-                        if string.find(fichier, 'Gravure') >= 0 :
-				sf.write("[%s]\n" % (filename))
-				fields = string.split(fichier, " ")
-				
-                                z = fields[0]
-
-				if string.find(filename, 'bis'):
-					n = string.replace(fields[2],"bis","")
-					bis = "R"
-				else:
-					n = fields[2]
-					bis = "--"
-
-                                print ("zone:",z, " numero:",n, " bis:",b) 
+            fichier = os.path.join(dirname, filename)
+            if os.path.isfile(fichier) :
+                # on decoupe le nom du fichier en z,n,b
+                if string.find(fichier, 'Gravure') >= 0 :
+            sf.write("[%s]\n" % (filename))
+            fields = string.split(fichier, " ")
+                z = fields[0]
+            if string.find(filename, 'bis'):
+                n = string.replace(fields[2],"bis","")
+                bis = "R"
+            else:
+                n = fields[2]
+                bis = "--"
+            print ("zone:",z, " numero:",n, " bis:",b)
 
 os.path.walk("/cdrom", parcours, (db, sf))
 sf.close()
--- archeobases/trunk/bego/utilitaires/recup/troche.py
+++ archeobases/trunk/bego/utilitaires/recup/troche.py
@@ -12,58 +12,48 @@
 # the Free Software Foundation's WEB site http://www.fsf.org
 #
 
-
 import sys
 import string
-
-sys.path.append("../cgi")
 import database
 
-db = database.DataBase(database="bego", username = "bego")
+
+sys.path.append("../cgi")
+db = database.DataBase(database="bego", username="bego")
 ######### avant d'inserer les rcohes, il faut creer les zones ##########
 for i in range (0, 23):
-	zone = "INSERT INTO zone(zone) VALUES(" + db.quote(i, "text") + ");"
-        db.query(zone)
-
+    zone = "INSERT INTO zone(zone) VALUES(" + db.quote(i, "text") + ");"
+    db.query(zone)
 lignes = sys.stdin.readlines()
 l = 0
 for ligne in lignes:
-	ligne = string.strip(ligne[:-1])
-	l = l + 1
-	if (ligne != "") and (ligne[:4] != "zone") and (ligne[:6] != "------")	:
-		champs = string.split(ligne, ";")
-		for i in range(0,len(champs)) :
-			champs[i] = string.strip(champs[i])
-		nouveaux = []
-		nouveaux.append(champs[0]) #z
-		nouveaux.append(champs[1]) #g
-
-		if champs[2] in ["-","--","---"] : #r
-			nouveaux.append(champs[3])
-		else :
-			nouveaux.append(champs[3]+champs[2])
-		nouveaux.append(champs[4]) #x
-		nouveaux.append(champs[5]) #y
-		nouveaux.append(champs[6]) #z
-
-		champs[7] = string.upper(champs[7])
-		if (champs[7] in ['G','GS','S','SG']) :
-			nouveaux.append(champs[7]) #nature
-		elif (champs[7] in ['SG']) :
-			nouveau.append('S')
-
-		for i in range(0, len(nouveaux)) :
-			if i  in [0, 2, 6] :
-				nouveaux[i] = db.quote(nouveaux[i], "text")
-
-		insert="INSERT INTO roche (zone, groupe, roche, x, y, z, nature) VALUES ("
-		insert = insert+string.join(nouveaux, ', ')
-		insert = insert + ");"
-
-
-		db.query(insert)
-
-		print l, insert
-
+    ligne = string.strip(ligne[:-1])
+    l = l + 1
+    if (ligne != "") and (ligne[:4] != "zone") and (ligne[:6] != "------"):
+        champs = string.split(ligne, ";")
+        for i in range(0, len(champs)):
+            champs[i] = string.strip(champs[i])
+        nouveaux = []
+        nouveaux.append(champs[0]) #z
+        nouveaux.append(champs[1]) #g
+        if champs[2] in ["-", "--", "---"]: #r
+            nouveaux.append(champs[3])
+        else:
+            nouveaux.append(champs[3]+champs[2])
+        nouveaux.append(champs[4]) #x
+        nouveaux.append(champs[5]) #y
+        nouveaux.append(champs[6]) #z
+        champs[7] = string.upper(champs[7])
+        if (champs[7] in ['G', 'GS', 'S', 'SG']):
+            nouveaux.append(champs[7]) #nature
+        elif (champs[7] in ['SG']):
+            nouveau.append('S')
+        for i in range(0, len(nouveaux)):
+            if i  in [0, 2, 6]:
+                nouveaux[i] = db.quote(nouveaux[i], "text")
+        insert = "INSERT INTO roche (zone, groupe, roche, x, y, z, nature) VALUES ("
+        insert = insert+string.join(nouveaux, ', ')
+        insert = insert + ");"
+        db.query(insert)
+        print l, insert
 db.query("VACUUM;")
 

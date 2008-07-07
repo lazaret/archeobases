e316edc3daf06f02abe15ab1caa0690c84a251d3
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2008-07-07 17:05:38 +0000

    Code cleaning to a more PEP8 one, see ticket #81

--- archeobases/trunk/collection/utilitaires/recup_excel/database.py
+++ archeobases/trunk/collection/utilitaires/recup_excel/database.py
@@ -12,7 +12,6 @@
 # the Free Software Foundation's WEB site http://www.fsf.org
 #
 
-
 import sys
 import os
 import time
@@ -26,54 +25,55 @@ import pg
 # Dans le cas de l'utilisation de cette classe dans un script CGI alors les
 # messages d'erreurs et de trace sont recuperables dans le fichier de log du
 # logiciel serveur WEB (teste uniquement avec Apache).
-class DataBase :
-        __database  = None
-        __debuglevel = 0
 
-        def __init__(self, host = None, database = None, username = None, debuglevel = 0) :
-                self.set_debug(debuglevel)
-                try :
-                        self.__database = pg.connect(host = host, dbname = database, user = username)
-                        if self.__debuglevel > 0 :
-                                self.sql_message("Connected to Host [%s] DataBase [%s] Username [%s]" % (host, database, username))
-                except pg.error, msg :
-                        self.fatal_message("Unable to connect to Host [%s] DataBase [%s] Username [%s] ==> [%s]" % (host, database, username, msg))
+class DataBase:
+    __database  = None
+    __debuglevel = 0
 
-        def log_message(self, msg, level) :
-                if os.environ.has_key("REMOTE_USER") :
-                        message = "[%s] [%s] [%s] %s\n" % (time.asctime(time.localtime(time.time())), level, os.environ["REMOTE_USER"], msg)
-                else :
-                        message = "[%s] [%s] %s\n" % (time.asctime(time.localtime(time.time())), level, msg)
-                sys.stderr.write(message)
-                sys.stderr.flush()
-                return message
+    def __init__(self, host=None, database=None, username=None, debuglevel=0):
+        self.set_debug(debuglevel)
+        try:
+            self.__database = pg.connect(host=host, dbname=database, user=username)
+            if self.__debuglevel > 0:
+                self.sql_message("Connected to Host [%s] DataBase [%s] Username [%s]" % (host, database, username))
+        except pg.error, msg:
+            self.fatal_message("Unable to connect to Host [%s] DataBase [%s] Username [%s] ==> [%s]" % (host, database, username, msg))
 
-        def sql_message(self, msg) :
-                return self.log_message(msg, level = "sql")
+    def log_message(self, msg, level):
+        if os.environ.has_key("REMOTE_USER"):
+            message = "[%s] [%s] [%s] %s\n" % (time.asctime(time.localtime(time.time())), level, os.environ["REMOTE_USER"], msg)
+        else:
+            message = "[%s] [%s] %s\n" % (time.asctime(time.localtime(time.time())), level, msg)
+        sys.stderr.write(message)
+        sys.stderr.flush()
+        return message
 
-        def error_message(self, msg) :
-                return self.log_message(msg, level = "error")
+    def sql_message(self, msg):
+        return self.log_message(msg, level="sql")
 
-        def fatal_message(self, msg) :
-                self.log_message(msg, "fatal")
-                sys.exit(-1)
+    def error_message(self, msg):
+        return self.log_message(msg, level="error")
 
-        def set_debug(self, debuglevel) :
-                self.__debuglevel = debuglevel
+    def fatal_message(self, msg):
+        self.log_message(msg, "fatal")
+        sys.exit(-1)
 
-        def quote(self, field, typ) :
-                return pg._quote(field, typ)
+    def set_debug(self, debuglevel):
+        self.__debuglevel = debuglevel
 
-        def query(self, q) :
-                if self.__database != None :
-                        if self.__debuglevel > 0 :
-                                self.sql_message(q)
-                        try :
-                                return self.__database.query(q)
-                        except pg.error, msg:
-                                if msg and (msg[-1] == '\n') :
-                                        msg = msg[:-1]
-                                self.__database.query("ROLLBACK;")      # auto rollback
-                                self.fatal_message("%s ==>> [%s]" % (q, msg))
-                else :
-                        self.error_message("No DataBase connection available")
+    def quote(self, field, typ):
+        return pg._quote(field, typ)
+
+    def query(self, q):
+        if self.__database != None:
+            if self.__debuglevel > 0:
+                self.sql_message(q)
+            try:
+                return self.__database.query(q)
+            except pg.error, msg:
+                if msg and (msg[-1] == '\n'):
+                    msg = msg[:-1]
+                self.__database.query("ROLLBACK;")      # auto rollback
+                self.fatal_message("%s ==>> [%s]" % (q, msg))
+        else:
+            self.error_message("No DataBase connection available")
--- archeobases/trunk/collection/utilitaires/recup_excel/excel2base.py
+++ archeobases/trunk/collection/utilitaires/recup_excel/excel2base.py
@@ -15,84 +15,74 @@
 # usage:
 # ./recup_biblio collection < fichier_recup_excel.csv
 
-
 import sys
 import string
-
-sys.path.append("../cgi")
-
 import database
 
-db = database.DataBase(database=sys.argv[1], username = "postgres")
-cpt=0
-cpt_ok =0
-pb=0
 
+sys.path.append("../cgi")
+db = database.DataBase(database=sys.argv[1], username = "postgres")
+cpt = 0
+cpt_ok = 0
+pb = 0
 lignes = sys.stdin.readlines()
 l = 0
 
 for ligne in lignes:
-        ligne = string.strip(ligne[:-1])
-
-        l = l + 1
-        champs = string.split(ligne, ";")
-        for i in range(0,len(champs)) :
-                champs[i] = string.strip(champs[i])
-                new_objet = []
-                new_biblio  = []
-                if (len(champs) == 11):
-                        
-                        new_biblio.append('DRET-BIB-' + champs[0])  #identifiant
-                        new_biblio.append('TIRE_A_PART')  #type_biblio
-                        new_biblio.append(champs[1])  #indice
-                        new_biblio.append(champs[2])  #auteur 
-                        new_biblio.append(champs[3])  #titre 
-                        new_biblio.append(champs[4])  #mots-clefs
-                        new_biblio.append(champs[5])  #revue
-                        new_biblio.append(champs[6])  #volume => description
-                        if (champs[7] <'1800' or champs[7] > '2010'):
-                                champs[7] = '0'
-                        new_biblio.append(champs[7])  #annee
-                        new_biblio.append(champs[8])  #pages  
-                        new_biblio.append(champs[9])  #nombre_exemplaire
-                        new_biblio.append(champs[10])  #langue
-                        new_biblio.append('ESSAI_INSERTION_TAP')         #resume  
-                        
-                        for i in range(0, len(new_biblio)):                
-                                new_biblio[i] = db.quote((new_biblio[i]), "text")
-                else:       
-                        pb = 1
-                        
-        try:
-                delete = "DELETE FROM biblio WHERE identifiant=" + new_biblio[0] + ";"
-                delete = delete.encode("UTF-8")  
-        except:
-                print "erreur delete"
-                        
-        if pb:
-                print ligne
-                print ("probleme longueur: ", len(champs))
-                pb = 0
+    ligne = string.strip(ligne[:-1])
+    l = l + 1
+    champs = string.split(ligne, ";")
+    for i in range(0, len(champs)) :
+        champs[i] = string.strip(champs[i])
+        new_objet = []
+        new_biblio  = []
+        if (len(champs) == 11):
+            new_biblio.append('DRET-BIB-' + champs[0])  #identifiant
+            new_biblio.append('TIRE_A_PART')  #type_biblio
+            new_biblio.append(champs[1])  #indice
+            new_biblio.append(champs[2])  #auteur
+            new_biblio.append(champs[3])  #titre
+            new_biblio.append(champs[4])  #mots-clefs
+            new_biblio.append(champs[5])  #revue
+            new_biblio.append(champs[6])  #volume => description
+            if (champs[7] <'1800' or champs[7] > '2010'):
+                champs[7] = '0'
+            new_biblio.append(champs[7])  #annee
+            new_biblio.append(champs[8])  #pages
+            new_biblio.append(champs[9])  #nombre_exemplaire
+            new_biblio.append(champs[10])  #langue
+            new_biblio.append('ESSAI_INSERTION_TAP')         #resume
+            for i in range(0, len(new_biblio)):
+                new_biblio[i] = db.quote((new_biblio[i]), "text")
         else:
-                q = "SELECT COUNT(*) FROM biblio "
-                q = q + "WHERE identifiant=" + new_biblio[0] + ";"
-                q = q.encode("UTF-8")
-                res = db.query(q)
-                res = res.dictresult()
-                res = res[0]["count"]
-        
-                if res:
-                        cpt = cpt + 1
-                else:
-                        insert_biblio = "INSERT INTO biblio (identifiant,b_type_biblio,b_indice,b_auteur,b_titre_ouvrage,b_mots_clefs,b_titre_periodique,b_description,b_annee,b_pages,b_nombre_ex_supp,b_langue_document,b_resume) VALUES ("
-                        insert_biblio = insert_biblio+string.join(new_biblio, ', ')
-                        insert_biblio = insert_biblio + ");"
-                        
-                        try:        
-                                db.query(insert_biblio)
-                                cpt_ok =cpt_ok + 1
-                        except:        
-                                print ("probleme ligne", insert_biblio)
+            pb = 1
+    try:
+        delete = "DELETE FROM biblio WHERE identifiant=" + new_biblio[0] + ";"
+        delete = delete.encode("UTF-8")
+    except:
+        print "erreur delete"
+    if pb:
+        print ligne
+        print ("probleme longueur: ", len(champs))
+        pb = 0
+    else:
+        q = "SELECT COUNT(*) FROM biblio "
+        q = q + "WHERE identifiant=" + new_biblio[0] + ";"
+        q = q.encode("UTF-8")
+        res = db.query(q)
+        res = res.dictresult()
+        res = res[0]["count"]
+        if res:
+            cpt = cpt + 1
+        else:
+            insert_biblio = "INSERT INTO biblio (identifiant,b_type_biblio,b_indice,b_auteur,b_titre_ouvrage,b_mots_clefs,b_titre_periodique,b_description,b_annee,b_pages,b_nombre_ex_supp,b_langue_document,b_resume) VALUES ("
+            insert_biblio = insert_biblio+string.join(new_biblio, ', ')
+            insert_biblio = insert_biblio + ");"
+            try:
+                db.query(insert_biblio)
+                cpt_ok = cpt_ok + 1
+            except:
+                print ("probleme ligne", insert_biblio)
 print(cpt, " doublons")
 print(cpt_ok, " OK")
 

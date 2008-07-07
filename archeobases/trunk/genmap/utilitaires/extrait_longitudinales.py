739f76ae11da6ea35a393096eb465808f32d5a2a
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2008-07-07 17:03:02 +0000

    Code cleaning to a more PEP8 one, see ticket #81

--- archeobases/trunk/genmap/utilitaires/extrait_longitudinales.py
+++ archeobases/trunk/genmap/utilitaires/extrait_longitudinales.py
@@ -12,7 +12,6 @@
 # the Free Software Foundation's WEB site http://www.fsf.org
 #
 
-
 import sys
 import string
 import pg
@@ -20,54 +19,50 @@ import pg
 
 # mode d'emploi
 # extrait_longitudinales.py nom_base zone_min zone_max ep_tranche
-    
-if __name__ == "__main__" :
-    if len(sys.argv) != 5 :
-        sys.stderr.write("usage : %s  base  zonemin  zonemax  epaisseur\n" % sys.argv[0])
-    else :
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.stderr.write("usage: %s  base  zonemin  zonemax  epaisseur\n" % sys.argv[0])
+    else:
         ma_base    = sys.argv[1]
         zone_min   = sys.argv[2].upper()
         zone_max   = sys.argv[3].upper()
         ep_tranche = int(sys.argv[4])
-        
-        requete = """SELECT zone, numero, bis, 
-                            x, y, zabsolu, nature, 
-                            longueur, largeur, epaisseur, 
+        requete = """SELECT zone, numero, bis,
+                            x, y, zabsolu, nature,
+                            longueur, largeur, epaisseur,
                             orientation, pendage
-                        FROM carnet    
+                        FROM carnet
                         WHERE zone LIKE '%s%%' AND x BETWEEN %i AND %i;"""
-                        
-        try :
+        try:
             db = pg.connect(dbname=ma_base, user="postgres")
-        except :    
+        except:
             sys.stderr.write("Impossible de se connecter à la base %s\n" % ma_base)
-        
-        for izone in range(ord(zone_min), ord(zone_max) + 1) :
+        for izone in range(ord(zone_min), ord(zone_max) + 1):
             zone = chr(izone)
             print "Extraction zone %s..." % zone
-            for x in range(int(100.0 / ep_tranche)) :
+            for x in range(int(100.0 / ep_tranche)):
                 print "\t%i" % (x*ep_tranche)
                 q = requete % (zone, x*ep_tranche, (x+1)*ep_tranche - 1)
                 resultat = db.query(q)
                 fichier = open("arago-%s-%02i.datas" % (zone, x*10), "w")
                 fichier.write("%s\n" % string.join(resultat.listfields(), ","))
-                for line in resultat.getresult() :
+                for line in resultat.getresult():
                     champs = []
-                    for field in line :
-                        try :
+                    for field in line:
+                        try:
                             float(field)
-                        except :    
+                        except:
                             # Chaine ou None (NULL), stocke en tant que 'None'
                             v = "'%s'" % field
-                        else :    
+                        else:
                             # Numerique entier ou flottant
                             v = str(field)
                         champs.append(v)
                     fichier.write("%s\n" % string.join(champs, ","))
                 fichier.close()
             print
-            
-        try :    
+        try:
             db.close()
-        except :     
+        except:
             pass # ignore les erreurs
--- archeobases/trunk/genmap/utilitaires/extrait_longitudinales_sauve.py
+++ archeobases/trunk/genmap/utilitaires/extrait_longitudinales_sauve.py
@@ -12,36 +12,34 @@
 # the Free Software Foundation's WEB site http://www.fsf.org
 #
 
-
-import sys
 import string
 import pg
 
-requete = """SELECT zone, numero, bis, 
-                    x, y, zabsolu, nature, 
-                    longueur, largeur, epaisseur, 
+
+requete = """SELECT zone, numero, bis,
+                    x, y, zabsolu, nature,
+                    longueur, largeur, epaisseur,
                     orientation, pendage
-                FROM carnet    
+                FROM carnet
                 WHERE zone LIKE '%s%%' AND x BETWEEN %i AND %i;"""
-                
 db = pg.connect(dbname="arago", user="postgres")
 
-for zone in "BCDEFGHIJKL" :
+for zone in "BCDEFGHIJKL":
     print "Extraction zone %s..." % zone
-    for x in range(10) :
+    for x in range(10):
         print "\t%i" % (x*10)
         q = requete % (zone, x*10, (x+1)*10 - 1)
         resultat = db.query(q)
         fichier = open("arago-%s-%02i.datas" % (zone, x*10), "w")
         fichier.write("%s\n" % string.join(resultat.listfields(), ","))
-        for line in resultat.getresult() :
+        for line in resultat.getresult():
             champs = []
-            for field in line :
-                try :
+            for field in line:
+                try:
                     v = float(field)
-                except :    
+                except:
                     v = "'%s'" % field
-                else :    
+                else:
                     v = str(field)
                 champs.append(v)
             fichier.write("%s\n" % string.join(champs, ","))

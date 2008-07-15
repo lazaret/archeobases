9b85466be92b2afccbf8b29f99ec219400d13686
Bertrand Lecervoisier <miniwark@miniwark.com>
2008-07-15 16:23:53 +0000

    Delete old unused scripts and move a few utilitary ones to the "utilitaires" folder. See ticket #77

--- archeobases/trunk/archeo/utilitaires/install/script_appli.py
+++ archeobases/trunk/archeo/utilitaires/install/script_appli.py
@@ -17,7 +17,7 @@
 # The scrip create the necessary directories and copy the files of
 # the Archeobases-Archeo project into the repertory /home/bases
 # from a /cdrom folder.
-# Please note than this scrip is depreciated and may not be udes anymore.
+# Please note than this scrip is depreciated and may not be used anymore.
 
 # import modules
 import os
@@ -36,45 +36,45 @@ if string.upper(premiere) == "O":
 
 #CREATION DES REPERTOIRES ET DROITS NECESSAIRES A L'APPLICATION
 #==============================================================
-	os.system("mkdir -p /home/bases/auth")
+    os.system("mkdir -p /home/bases/auth")
 
-	os.system("mkdir -p /home/bases/archeo/decors")
-	os.system("cp -R /cdrom/decors/* /home/bases/archeo/")
+    os.system("mkdir -p /home/bases/archeo/decors")
+    os.system("cp -R /cdrom/decors/* /home/bases/archeo/")
 
-	os.system("mkdir -p /home/bases/archeo/lexique")
-	os.system("cp -R /cdrom/help/* /home/bases/archeo/")
+    os.system("mkdir -p /home/bases/archeo/lexique")
+    os.system("cp -R /cdrom/help/* /home/bases/archeo/")
 
-	os.system("mkdir -p /home/bases/archeo/manuel")
-	os.system("cp -R /cdrom/manuel/* /home/bases/archeo/")
+    os.system("mkdir -p /home/bases/archeo/manuel")
+    os.system("cp -R /cdrom/manuel/* /home/bases/archeo/")
 
 # COPIE DES PROGRAMMES DE L'APPLICATION
 #======================================
-	os.system("mkdir -p /home/bases/cgi-bin")
-	os.system("cp /cdrom/prog_cgi/* /home/bases/archeo/cgi-bin")
-	os.system("chmod +x /home/bases/cgi-bin/archeo/*py")
+    os.system("mkdir -p /home/bases/cgi-bin")
+    os.system("cp /cdrom/prog_cgi/* /home/bases/archeo/cgi-bin")
+    os.system("chmod +x /home/bases/cgi-bin/archeo/*py")
 
 # FAIRE LES LIENS SYMBOLIQUE DANS /usr/lib/cgi
 #=============================================
-	os.system ("mkdir -p /usr/lib/cgi-bin")
-	os.system ("ln -s /home/bases/archeo/cgi-bin /usr/lib/cgi-bin/archeo")
-	os.system ("ln -s /home/bases/archeo/cgi-bin/archeo.py /usr/lib/cgi-binarcheo.py")
+    os.system ("mkdir -p /usr/lib/cgi-bin")
+    os.system ("ln -s /home/bases/archeo/cgi-bin /usr/lib/cgi-bin/archeo")
+    os.system ("ln -s /home/bases/archeo/cgi-bin/archeo.py /usr/lib/cgi-binarcheo.py")
 
 # CREER LE FICHIER D'AUTHENTIFICATION ET Y AJOUTER LES UTILISATEURS ET MOTS DE PASSE
 #==============================================================================
-	os.system ("mkdir -p /home/bases/auth")
+    os.system ("mkdir -p /home/bases/auth")
 
 
 # CREER LE REPERTOIRE IMAGE S'IL N'EXISTE PAS ENCORE
 # Y AJOUTER LE NOM DE LA BASE ET METTRE LES DROITS A www-data www-data
 #=====================================================================
-	os.system("htpasswd -c authentification super%s"%sys.argv[1])
-	os.system("htpasswd  authentification %s"%sys.argv[1])
-	os.system("htpasswd  authentification visiteur%s"%sys.argv[1])
+    os.system("htpasswd -c authentification super%s"%sys.argv[1])
+    os.system("htpasswd  authentification %s"%sys.argv[1])
+    os.system("htpasswd  authentification visiteur%s"%sys.argv[1])
 
 else:
-	os.system("htpasswd  authentification super%s"%sys.argv[1])
-	os.system("htpasswd  authentification %s"%sys.argv[1])
-	os.system("htpasswd  authentification visiteur%s"%sys.argv[1])
+    os.system("htpasswd  authentification super%s"%sys.argv[1])
+    os.system("htpasswd  authentification %s"%sys.argv[1])
+    os.system("htpasswd  authentification visiteur%s"%sys.argv[1])
 
 
 os.system("mkdir /home/bases/archeo/%s/images"%nom_base)

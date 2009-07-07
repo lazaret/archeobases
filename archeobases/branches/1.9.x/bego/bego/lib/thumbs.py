3f9d3a6341345b6ab661219aba40d0cc00ae151d
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2009-07-07 19:56:38 +0000

    Move thumbnail generation into a middleware

---
+++ archeobases/branches/1.9.x/bego/bego/lib/thumbs.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from pylons import config
+
+from iw.thumbs.middleware import Thumbs
+
+
+def add_thumbs(app):
+    """
+    Add image thumbnails generation middleware to the ``app``.
+    """
+
+    preview = (560,480)
+    sidebox = (170,255)
+
+    return Thumbs(app,
+                  image_dir = config['app_conf']['image_dir'],
+                  cache_dir=os.path.join(config['app_conf']['cache_dir'], 'thumbs'),
+                  url_regexp = '/thumbs/(?P<size>%s)(?P<path>/.*)',
+                  url_parser = 'iw.thumbs.url:size_parser',
+                  sizes = {'preview': preview, 'sidebox': sidebox})
+
+
+

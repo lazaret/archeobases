8f2bf50559ac9b987972caeee6e309d7b618fd8c
Bertrand Lecervoisier <miniwark@miniwark.com>
2009-07-08 17:33:32 +0000

    testing

--- archeobases/branches/1.9.x/build_scripts/bego_build.py
+++ archeobases/branches/1.9.x/build_scripts/bego_build.py
@@ -1,8 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Create a virtualenv environment and install packages mandatory
-for the building and testing of the Bego"""
+"""Build script used by the Trac/Bitten build tool"""
 
 # This is just a convenien scripts i use with Bitten build tool
 # to prepare silently an isolated environement with the necessary
@@ -19,38 +18,41 @@ for the building and testing of the Bego"""
 # repoze.what-quickstart
 # Python Imaging Library
 # iw.thumbs
-# Webhelpers developement version
 
 
-__version__ = "0.1"
+__version__ = "0.2"
 
-import os
 import subprocess
+from optparse import OptionParser
+
 
 def install_deps():
     """Install the project dependencies for build and test purpose"""
+    print "Setup nose, Pylons, SQLAlchemy, repoze.what, PIL, iw.thumbs"
     devnull = open(os.devnull, 'w')
-    result = subprocess.call('env/bin/easy_install nose', shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('env/bin/easy_install Pylons', shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('env/bin/easy_install SQLalchemy', shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('env/bin/easy_install repoze.what-pylons', shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('env/bin/easy_install repoze.what-quickstart', shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('wget http://effbot.org/downloads/Imaging-1.1.6.tar.gz',
-                            shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('tar zxvf Imaging-1.1.6.tar.gz', shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('env/bin/python Imaging-1.1.6/setup.py install', shell=True, stdout=devnull, stderr=devnull)
-    result = subprocess.call('env/bin/easy_install iw.thumbs', shell=True, stdout=None, stderr=devnull)
-    result = subprocess.call('env/bin/easy_install http://bitbucket.org/bbangert/webhelpers/get/8658ea32d22a.gz',
+    result = subprocess.call("env/bin/easy_install nose", shell=True, stdout=devnull, stderr=devnull)
+    result = subprocess.call("env/bin/easy_install Pylons", shell=True, stdout=devnull, stderr=devnull)
+    result = subprocess.call("env/bin/easy_install SQLalchemy", shell=True, stdout=devnull, stderr=devnull)
+    result = subprocess.call("env/bin/easy_install repoze.what-pylons", shell=True, stdout=devnull, stderr=devnull)
+    result = subprocess.call("env/bin/easy_install repoze.what-quickstart", shell=True, stdout=devnull, stderr=devnull)
+    # TODO : comment this when webhelper 1.0 will be stable
+    result = subprocess.call("env/bin/easy_install http://bitbucket.org/bbangert/webhelpers/get/8658ea32d22a.gz",
                             shell=True, stdout=None, stderr=devnull)
+    result = subprocess.call("wget http://effbot.org/downloads/Imaging-1.1.6.tar.gz",
+                            shell=True, stdout=devnull, stderr=devnull)
+    result = subprocess.call("tar zxvf Imaging-1.1.6.tar.gz", shell=True, stdout=devnull, stderr=devnull)
+    result = subprocess.call("env/bin/python Imaging-1.1.6/setup.py install", shell=True, stdout=devnull, stderr=devnull)
+    result = subprocess.call("env/bin/easy_install iw.thumbs", shell=True, stdout=None, stderr=devnull)
     devnull.close()
-    print "test"
 
 
 def main():
     """Create a virtualenv environment and install the
     project dependencies"""
-    install_deps()
-
+    parser = OptionParser(version="%prog - "+__version__, description="Build script used by the Bitten build tool.")
+    parser.add_option("-i", "--install-deps", action="callback", callback=install_deps,
+                     help="Install the project dependencies")
+    (options, args) = parser.parse_args()
 
 if __name__ == "__main__":
     main()

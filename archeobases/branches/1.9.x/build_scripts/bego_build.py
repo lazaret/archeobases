b605bbe19712ce5859105724c902c750e1d6f1c4
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2009-07-08 15:21:48 +0000

    Buid_script for use with bitten

---
+++ archeobases/branches/1.9.x/build_scripts/bego_build.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Create a virtualenv environment and install packages mandatory
+for the building and testing of the Bego"""
+
+# This is just a convenien scripts i use with Bitten build tool
+# to prepare silently an isolated environement with the necessary
+#packages of the Bego Project.
+#
+# This script create an 'env' virtual environment
+# This script install the following packages in the isolated environment
+# nose
+# Pylons
+# SQLalchemy
+# repoze.what-pylons
+# repoze.what-quickstart
+# Python Imaging Library
+# iw.thumbs
+# Webhelpers developement version
+

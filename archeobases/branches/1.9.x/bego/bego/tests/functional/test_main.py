80b78f3c232865673fe6647e732b0671609a612d
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2009-07-10 15:43:31 +0000

    Remove Main controler witch have been replaced by Root controler

--- archeobases/branches/1.9.x/bego/bego/tests/functional/test_main.py
+++
@@ -1,7 +0,0 @@
-from bego.tests import *
-
-class TestMainController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='main', action='index'))
-        # Test response...

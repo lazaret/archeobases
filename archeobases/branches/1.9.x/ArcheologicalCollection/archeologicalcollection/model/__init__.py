6670ed936a1a9ca5579f4fff7243a4455e09c7fe
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2009-07-21 18:38:39 +0000

    Initial commit of a Pylons stubb to work on

---
+++ archeobases/branches/1.9.x/ArcheologicalCollection/archeologicalcollection/model/__init__.py
@@ -0,0 +1,21 @@
+"""The application's model objects"""
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from archeologicalcollection.model import meta
+
+def init_model(engine):
+    """Call me before using any of the tables or classes in the model"""
+    ## Reflected tables must be defined and mapped here
+    #global reflected_table
+    #reflected_table = sa.Table("Reflected", meta.metadata, autoload=True,
+    #                           autoload_with=engine)
+    #orm.mapper(Reflected, reflected_table)
+    #
+    meta.Session.configure(bind=engine)
+    meta.engine = engine
+
+
+# import tables models
+from archeologicalcollection.model.auth import User, Group, Permission

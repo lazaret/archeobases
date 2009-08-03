33aa8c4705bc93338ca5ed0d5f299160879321b2
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2009-08-03 14:29:03 +0000

    Move middleware initialisation variables to private

--- archeobases/branches/1.9.x/bego/bego/lib/thumbs.py
+++ archeobases/branches/1.9.x/bego/bego/lib/thumbs.py
@@ -1,5 +1,7 @@
-""" Image thumbnail generation middleware."""
+"""Image thumbnail generation middleware.
 
+Used to genrate on the fly image thumbnails.
+"""
 import os
 
 from pylons import config
@@ -10,15 +12,16 @@ from iw.thumbs.middleware import Thumbs
 def add_thumbs(app):
     """Add the image thumbnails generation middleware to the ``app``."""
 
-    preview = (560, 480)
-    sidebox = (170, 255)
+    _preview = (560, 480)
+    _sidebox = (170, 255)
+    _url_regexp = '/thumbs/(?P<size>%s)(?P<path>/.*)'
 
     return Thumbs(app,
                   image_dir = config['app_conf']['image_dir'],
                   cache_dir=os.path.join(config['app_conf']['cache_dir'], 'thumbs'),
-                  url_regexp = '/thumbs/(?P<size>%s)(?P<path>/.*)',
-                  url_parser = 'iw.thumbs.url:size_parser',
-                  sizes = {'preview': preview, 'sidebox': sidebox})
+                  url_regexp = _url_regexp,
+                  url_parser = "iw.thumbs.url:size_parser",
+                  sizes = {'preview': _preview, 'sidebox': _sidebox})
 
 
 

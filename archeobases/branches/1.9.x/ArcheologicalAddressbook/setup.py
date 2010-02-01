83eddb6636fce812d8af66c117ed21dd6353e9f4
corinne <corinne@dee540c9-f427-0410-9603-694751ae893f>
2010-02-01 09:48:41 +0000

    typos

--- archeobases/branches/1.9.x/ArcheologicalAddressbook/setup.py
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""  Install script for The `ArchaeologicalAdressbook` package."""
+""" Install script for The `ArchaeologicalAdressbook` package."""
 
 try:
     from setuptools import setup, find_packages
@@ -11,19 +11,19 @@ except ImportError:
 setup(
     name='ArcheologicalAddressbook',
     version='1.9.a1',
-    description="Web application for contact adress management \
-                 specific to archaelogical excavations and volontary association'.",
+    description='Web application for contact adress management \
+                 specific to archaelogical excavations and volontary association.',
     author='LDPL - Laboratoire Départemental de Préhistoire du Lazaret',
     author_email='opensource@lazaret.unice.fr',
     url='http://lazaret.unice.fr/opensource/',
     install_requires=[
-        "Pylons>=0.9.7",
-        "SQLAlchemy>=0.5.8",
-        "Mako>=0.2.4",
-        "repoze.what-pylons>=1.0",
-        "repoze.what-quickstart>=1.0.5",
+        'Pylons>=0.9.7',
+        'SQLAlchemy>=0.5.8',
+        'Mako>=0.2.4',
+        'repoze.what-pylons>=1.0',
+        'repoze.what-quickstart>=1.0.5',
     ],
-    setup_requires=["PasteScript>=1.7.3"],
+    setup_requires=['PasteScript>=1.7.3'],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     test_suite='nose.collector',

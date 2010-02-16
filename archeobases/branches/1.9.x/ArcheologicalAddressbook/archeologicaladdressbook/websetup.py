9e3d773db9387002826468f4dfa974a5a8ee64a7
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2010-02-16 10:47:08 +0000

    Update Auth model and add  default admin user, group and permission

--- archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/websetup.py
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/websetup.py
@@ -2,6 +2,7 @@
 import logging
 
 from archeologicaladdressbook.config.environment import load_environment
+from archeologicaladdressbook import model
 from archeologicaladdressbook.model import meta
 
 log = logging.getLogger(__name__)
@@ -14,3 +15,31 @@ def setup_app(command, conf, vars):
     log.info("Creating tables")
     meta.metadata.create_all(bind=meta.engine)
     log.info("Table creation done.")
+
+    # Add default values in the tables
+    log.info("Add default user, group and permission")
+
+    u = model.User()
+    u.user_name = u'manager'
+    u.display_name = u'Example manager'
+    u.email_address = u'manager@somedomain.com'
+    u.password = u'managepass'
+    meta.Session.add(u)
+
+    g = model.Group()
+    g.group_name = u'managers'
+    g.display_name = u'Managers Group'
+    g.users.append(u)
+    meta.Session.add(g)
+
+    p = model.Permission()
+    p.permission_name = u'manage'
+    p.description = u'This permission give an administrative right to the bearer'
+    p.groups.append(g)
+    meta.Session.add(p)
+
+    meta.Session.flush()
+    meta.Session.commit()
+    log.info("Adding default user, group and permission done.")
+
+

12c8c32ce1085ea71db5cc0d0ce61f7a03ada22f
corinne <corinne@dee540c9-f427-0410-9603-694751ae893f>
2010-02-16 13:45:17 +0000

    Adding the model directory on the server.

---
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/model/address.py
@@ -0,0 +1,22 @@
+"""SQLAlchemy model definition for addresses"""
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from archeologicaladdressbook.model import meta
+
+
+class Address(meta.DeclarativeBase):
+    """address table model"""
+    __tablename__ = 'address'
+    
+    address_id = sa.Column(sa.types.Integer, autoincrement=True, primary_key=True)
+    person_id = sa.Column(sa.types.Integer, sa.ForeignKey('person.person_id'))
+    address_line1 = sa.Column(sa.types.Unicode(100), nullable=False)
+    address_line2 = sa.Column(sa.types.Unicode(100))
+    address_line3 = sa.Column(sa.types.Unicode(100))
+    zip_code = sa.Column(sa.types.Unicode(15), nullable=False)
+    city = sa.Column(sa.types.Unicode(25), nullable=False)
+    country = sa.Column(sa.types.Unicode(25))
+    # Type: home/business/correspondent
+    address_type = sa.Column(sa.types.Unicode(13), nullable=False)

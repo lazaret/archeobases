8b0707da8c0083ac677ba4ecb29fb96f2872b252
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2010-02-26 15:35:48 +0000

    Basic functional test

---
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/tests/functional/test_admin.py
@@ -0,0 +1,12 @@
+"""Root controler fonctional tests"""
+
+from archeologicaladdressbook.tests import *
+
+
+class TestAdminController(TestController):
+
+    def test_index(self):
+        """Test the url of the admin page"""
+        response = self.app.get(url('admin'))
+        # Test response...
+

c3efe3ad7fa3d7994792f976a72db5d638d5f342
Bertrand Lecervoisier <miniwark@miniwark.com>
2010-03-03 12:11:41 +0000

    Test fixtures for the model

---
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/tests/model/fixtures/person.py
@@ -0,0 +1,28 @@
+""" Datas used for testing the `Person` model of the application."""
+
+
+import datetime
+
+
+class PersonData:
+    """ Datas used for testing the `Person` model."""
+
+    class john_doe:
+        last_name = u'Doe'
+        first_name = u'John'
+        title = u'Mr'
+        birth_date = datetime.date(1969, 9, 6)
+        activity = u'Test person 1'
+        person_type = u'Voluntary Member'
+
+
+class DuplicatePersonData:
+    """ Datas used for testing unique constrain on the `Person` model."""
+
+    class john_doe:
+        last_name = PersonData.john_doe.last_name
+        first_name = PersonData.john_doe.first_name
+        title = u'Mrs'
+        birth_date = PersonData.john_doe.birth_date
+        activity = u'Test person 2'
+        person_type = u'Voluntary Member'

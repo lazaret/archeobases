74761d950d536c3e5fd14b8107d632473f7dbe18
Bertrand Lecervoisier <miniwark@miniwark.com>
2010-09-21 09:59:27 +0000

    Add FormEncode schema for person

---
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/model/forms/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 LDLP (Laboratoire Départemental de Prehistoire du Lazaret)
+# http://lazaret.unice.fr/opensource/ - opensource@lazaret.unice.fr
+#
+# This file is part of ArcheologicalAdressbook and is released under
+# the GNU Affero General Public License 3 or any later version.
+# See LICENSE.txt or <http://www.gnu.org/licenses/agpl.html>
+#
+""" The application's form schemas."""
+
+# import tables classes
+from archeologicaladdressbook.model.forms.person import PersonForm
+

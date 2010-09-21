49e86788bb68b369c3653fb858908e36a0d920eb
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2010-09-21 08:53:41 +0000

    Update dbadmin and auth

--- archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/lib/auth.py
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/lib/auth.py
@@ -9,8 +9,13 @@
 #
 """ Auth & Auth middleware."""
 
-from repoze.what.plugins.quickstart import setup_sql_auth
+from pylons import response
+from pylons.controllers.util import abort
 
+from repoze.what.plugins.quickstart import setup_sql_auth
+from repoze.what.plugins.pylonshq import ActionProtector, ControllerProtector
+
+from archeologicaladdressbook.lib.helpers import flash_message
 from archeologicaladdressbook.model.meta import Session
 from archeologicaladdressbook.model.auth import User, Group, Permission
 
@@ -24,5 +29,36 @@ def add_auth(app):
     return setup_sql_auth(app, User, Group, Permission, Session,
                           logout_handler='/logout',
                           post_login_url='/post_login',
-                          post_logout_url='/post_logout')
+                          post_logout_url='/post_logout',
+                          charset='utf-8')
 
+
+def denial_handler(reason):
+    # When this handler is called, response.status has two possible values:
+    # 401 or 403.
+    if response.status_int == 401:
+        message = 'Oops, you have to login: %s' % reason
+        message_type = 'warning'
+    else:
+        credentials = request.environ.get('repoze.what.credentials')
+        userid = credentials['repoze.what.userid']
+        message = "Come on, %s, you know you can't do that: %s" % (userid,
+                                                                   reason)
+        message_type = 'error'
+    flash_message(message, message_type)
+    abort(response.status_int, comment=reason)
+
+
+
+class protect_action(ActionProtector):
+    """ Decorator used to protect actions."""
+    # Our denial handler should flash the denial reason:
+    default_denial_handler = staticmethod(denial_handler)
+
+
+class protect_controller(ControllerProtector):
+    """ Decorator used to protect controllers."""
+    protector = protect_action
+
+
+#TODO : check and refactor denial handler

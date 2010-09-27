c06966dc7880220ddc3df7f640f80815406c838d
Bertrand Lecervoisier <miniwark@miniwark.com>
2010-09-27 08:22:03 +0000

    Update docstring and move the publication of Auth & Auth credentials to the BaseController

--- archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/controllers/root.py
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/controllers/root.py
@@ -28,13 +28,6 @@ log = logging.getLogger(__name__)
 class RootController(BaseController):
     """Main controler witch serve the main index page."""
 
-    def __before__(self):
-        credentials = request.environ.get('repoze.what.credentials', False)
-        if credentials:
-            c.userid = credentials['repoze.what.userid']
-        else:
-            c.userid = None
-
     def index(self):
         """ Render the main index page."""
         c.came_from = request.params.get('came_from') or url('/')
@@ -48,6 +41,11 @@ class RootController(BaseController):
         redirect(url(controller='root', action='index', came_from=came_from))
 
     def post_login(self):
+        """ Post login action.
+
+        If the credentials are good the user is connected and redirect to the
+        page he comme from. If they are wrong the user his resend to login.
+        """
         came_from = str(request.params.get('came_from', '')) or url('/')
         credentials = request.environ.get('repoze.what.credentials', False)
         if credentials:
@@ -59,22 +57,23 @@ class RootController(BaseController):
             redirect(url('/login', came_from=came_from))
 
     def post_logout(self):
+        """ Post logout action."""
         flash_message('You have been logout !')
         redirect(url(controller='root', action='index'))
 
 
 
 ################# tests ####################### TODO REMOVE BELLOW
-
-    @protect_action(is_user('manager', msg=_('Authentification required')))
-    def test(self):
-        flash_message('protected page !')
-        return render('/index.mako')
-
-
-    def environ(self):
-        result = '<html><body><h1>Environ</h1>'
-        for key, value in request.environ.items():
-            result += '%s: %r <br />' %(key, value)
-        result += '</body></html>'
-        return result
+#
+#    @protect_action(is_user('manager', msg=_('Authentification required')))
+#    def test(self):
+#        flash_message('protected page !')
+#        return render('/index.mako')
+#
+#
+#    def environ(self):
+#        result = '<html><body><h1>Environ</h1>'
+#        for key, value in request.environ.items():
+#            result += '%s: %r <br />'% (key, value)
+#        result += '</body></html>'
+#        return result

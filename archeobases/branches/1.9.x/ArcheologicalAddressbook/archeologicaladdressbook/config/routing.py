eebdb450b0d545582366921d705972934b051967
Bertrand Lecervoisier <miniwark@miniwark.com>
2010-09-29 13:20:45 +0000

    Finaly use built in "map" and "id" like in the default Pylons templates

--- archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/config/routing.py
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/config/routing.py
@@ -19,34 +19,34 @@ from routes import Mapper
 
 def make_map(config):
     """ Create, configure and return the routes Mapper."""
-    mapper = Mapper(directory=config['pylons.paths']['controllers'],
+    map = Mapper(directory=config['pylons.paths']['controllers'],
                  always_scan=config['debug'])
-    mapper.minimization = False
-    mapper.explicit = False
+    map.minimization = False
+    map.explicit = False
 
     # The ErrorController route (handles 404/500 error pages); it should
     # likely stay at the top, ensuring it can always be resolved
-    mapper.connect('/error/{action}', controller='error')
-    mapper.connect('/error/{action}/{id}', controller='error')
+    map.connect('/error/{action}', controller='error')
+    map.connect('/error/{action}/{id}', controller='error')
 
     # CUSTOM ROUTES HERE
 
     # FormAlechemy `DbmadminController` controller routes
-    mapper.connect('fa_static', '/dbadmin/_static/{path_info:.*}', controller='dbadmin', action='static')
-    mapper.connect('dbadmin', '/dbadmin', controller='dbadmin', action='models')
-    mapper.connect('formatted_admin', '/dbadmin.json', controller='dbadmin', action='models', format='json')
-    mapper.resource('model', 'models', path_prefix='/dbadmin/{model_name}', controller='dbadmin')
+    map.connect('fa_static', '/dbadmin/_static/{path_info:.*}', controller='dbadmin', action='static')
+    map.connect('dbadmin', '/dbadmin', controller='dbadmin', action='models')
+    map.connect('formatted_admin', '/dbadmin.json', controller='dbadmin', action='models', format='json')
+    map.resource('model', 'models', path_prefix='/dbadmin/{model_name}', controller='dbadmin')
 
     # Default `index` action for controllers
-    mapper.connect('/{controller}', action='index')
-    mapper.connect('/{controller}/', action='index')
+    map.connect('/{controller}', action='index')
+    map.connect('/{controller}/', action='index')
 
-    # `Root` controller actions (index, login, logout, etc...)
-    mapper.connect('/', controller='root', action='index')
-    mapper.connect('/{action}', controller='root')
+    # `RootController` controller actions (index, login, logout, etc...)
+    map.connect('/', controller='root', action='index')
+    map.connect('/{action}', controller='root')
 
     # Default controller/action routes
-    mapper.connect(None, '/{controller}/{action}')
-    mapper.connect(None, '/{controller}/{action}/{id}')
+    map.connect(None, '/{controller}/{action}')
+    map.connect(None, '/{controller}/{action}/{id}')
 
-    return mapper
+    return map

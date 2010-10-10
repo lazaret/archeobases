5b32728226eb9aec2e112995e23c969f57c26e5b
Bertrand Lecervoisier <miniwark@miniwark.com>
2010-10-10 09:48:01 +0000

    extend auth tests

--- archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/tests/functional/test_persons.py
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/tests/functional/test_persons.py
@@ -17,7 +17,7 @@ class TestPersonsController(TestController):
 
     editor = {'repoze.what.userid': u'editor',
                    'groups': (u'editors'),
-                   'permissions': (u'edit',u'view')}
+                   'permissions': (u'edit', u'view')}
 
     guest = {'repoze.what.userid': u'guest',
                    'groups': (u'guests'),
@@ -36,54 +36,68 @@ class TestPersonsController(TestController):
         self.app.get(url(controller='persons', action='delete'))
         self.app.get(url(controller='persons', action='confirm_delete'))
 
-    def test_2_anonymous_denied(self):
+    def test_2_controller_denied_for_anonymous(self):
         """ Test than the `PersonsController` controller is denied to anonymous."""
         # status 302 and not 401 because denied users are redirected to the main page
         self.app.get(url(controller='persons'), status=302)
 
-    #TODO separate 'view' and 'edit' permissions tests (and update templates)
-    def test_3_edit_allowed(self):
+    def test_3_controller_allowed_for_editors(self):
         """ Test than the `PersonsController` controller is allowed for users with 'edit' permission."""
         self.app.get(url(controller='persons'),
             extra_environ={'repoze.what.credentials': self.editor},
             status=200)
 
-    def test_4_view_allowed(self):
+    def test_4_controller_allowed_for_viewers(self):
         """ Test than the `PersonsController` controller is allowed for users with 'view' permission."""
         self.app.get(url(controller='persons'),
             extra_environ={'repoze.what.credentials': self.guest},
             status=200)
 
-    def test_5_index_response(self):
+    def test_5_new_denied_for_viewers(self):
+        """ Test than `new` action of the `PersonsController` is denied for users with 'view' permission."""
+        self.app.get(url(controller='persons', action='new'),
+            extra_environ={'repoze.what.credentials': self.guest},
+            status=302)
+
+    def test_6_edit_denied_for_viewers(self):
+        """ Test than `edit` action of the `PersonsController` is denied for users with 'view' permission."""
+        self.app.get(url(controller='persons', action='edit'),
+            extra_environ={'repoze.what.credentials': self.guest},
+            status=302)
+
+    def test_7_delete_denied_for_viewers(self):
+        """ Test than `confirm_delete` action of the `PersonsController` is denied for users with 'view' permission."""
+        self.app.get(url(controller='persons', action='confirm_delete'),
+            extra_environ={'repoze.what.credentials': self.guest},
+            status=302)
+
+    def test_8_index_response(self):
         """ Test response of the `PersonsController` index page."""
         response = self.app.get(url(controller='persons', action='index'),
             extra_environ={'repoze.what.credentials': self.editor})
         assert 'persons index template' in response
 
-#TODO add fixture for this test
-#    def test_5_edit_response(self):
+#    def test_9_edit_response(self): #TODO add a database fixture
 #        """ Test response of the `PersonsController` edit page."""
 #        response = self.app.get(url(controller='persons', action='edit'),
 #            extra_environ={'repoze.what.credentials': self.editor})
 #        assert 'persons edit template' in response
 
-    def test_6_list_response(self):
+    def test_10_list_response(self):
         """ Test response of the `PersonsController` list page."""
         response = self.app.get(url(controller='persons', action='list'),
             extra_environ={'repoze.what.credentials': self.editor})
         assert 'persons list template' in response
 
-    def test_7_new_response(self):
+    def test_11_new_response(self):
         """ Test response of the `PersonsController` new page."""
         response = self.app.get(url(controller='persons', action='new'),
             extra_environ={'repoze.what.credentials': self.editor})
         assert 'persons new template' in response
 
-#TODO add fixture for this test
-#    def test_8_show_response(self):
+#    def test_12_show_response(self): #TODO add a database fixture
 #        """ Test response of the `PersonsController` show page."""
 #        response = self.app.get(url(controller='persons', action='show'),
 #            extra_environ={'repoze.what.credentials': self.editor})
 #        assert 'persons show template' in response
 
-#TODO add more auth tests -> view + edit

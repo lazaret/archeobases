0029dda1ea0f65890faadab6d1c94f0ee1d60134
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2010-10-11 18:56:34 +0000

    Update flash message and login-box css styles

--- archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/public/javascripts/main.js
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/public/javascripts/main.js
@@ -16,6 +16,7 @@ $(function(){
     $("#login-link").click(function(){
     if ($("#login-panel").is(':hidden')){
         $("#login-panel").slideDown(200);
+        $("#login-link").css("padding-bottom","8px");
         $("#login-link").css("-moz-border-radius","0");
         $("#login-link").css("-webkit-border-radius","0");
         $("#login-link").css("-khtml-border-radius","0");
@@ -40,11 +41,11 @@ $(function(){
 
 
 
-// hide flash messages on close for all and after 5 for success and notice ones
+// hide flash messages on close for all and after 4 for success and notice ones
 $(function(){
-    // slide up #success_message" and #notice_message messages after 5 seconds
-    $("#success_message").delay(5000).slideUp(250);
-    $("#notice_message").delay(5000).slideUp(250);
+    // slide up #success_message" and #notice_message messages after 4 seconds
+    $("#success_message").delay(4000).slideUp(250);
+    $("#notice_message").delay(4000).slideUp(250);
     // slide up messages if closed
     $("div a.close_message").click(function(){
         //stop is used to remove the delay if there is one
@@ -68,7 +69,6 @@ $.datepicker.setDefaults({
 
 
 // display a date picker for date fields
-// TODO HTML5 support-fallback
 $(function() {
     $("#birth_date").datepicker();
 });

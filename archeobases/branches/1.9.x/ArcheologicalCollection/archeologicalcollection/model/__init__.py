59098e8cbf99e80a96b1048ec48cbaece4af2c5b
Bertrand Lecervoisier <miniwark@miniwark.com>
2010-10-15 14:06:28 +0000

    Update model and egg-info files

--- archeobases/branches/1.9.x/ArcheologicalCollection/archeologicalcollection/model/__init__.py
+++ archeobases/branches/1.9.x/ArcheologicalCollection/archeologicalcollection/model/__init__.py
@@ -5,18 +5,14 @@ from sqlalchemy import orm
 
 from archeologicalcollection.model import meta
 
+
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
-    ## Reflected tables must be defined and mapped here
-    #global reflected_table
-    #reflected_table = sa.Table("Reflected", meta.metadata, autoload=True,
-    #                           autoload_with=engine)
-    #orm.mapper(Reflected, reflected_table)
-    #
+
     meta.Session.configure(bind=engine)
     meta.engine = engine
 
 
-# import tables models
-#from archeologicalcollection.model.auth import User, Group, Permission
+# import tables classes
+from archeologicalcollection.model.auth import User, Group, Permission
 

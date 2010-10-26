b29c35234f8d978c24c8b69cc06d567f131c9847
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2010-10-26 15:23:42 +0000

    Update Formencode validation

---
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/lib/converters.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 LDLP (Laboratoire Départemental de Prehistoire du Lazaret)
+# http://lazaret.unice.fr/opensource/ - opensource@lazaret.unice.fr
+#
+# This file is part of ArcheologicalAdressbook and is released under
+# the GNU Affero General Public License 3 or any later version.
+# See LICENSE.txt or <http://www.gnu.org/licenses/agpl.html>
+#
+""" Converters fuctions.
+
+consist of functions to convert text to typically be used within
+"""
+
+def lowercase(value):
+    """ Return the lowercase of `value`"""
+    return value.lower()
+
+
+def uppercase(value):
+    """ Return the lowercase of `value`"""
+    return value.upper()

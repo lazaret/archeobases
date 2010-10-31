514209f7561c99662a8edb8f1cae759a07ed0213
Bertrand Lecervoisier <miniwark@miniwark.com>
2010-10-31 19:25:19 +0000

    Add basic tests for Auth model

---
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/tests/model/test_permissions.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 LDLP (Laboratoire Départemental de Prehistoire du Lazaret)
+# http://lazaret.unice.fr/opensource/ - opensource@lazaret.unice.fr
+#
+# This file is part of ArcheologicalAdressbook and is released under
+# the GNU Affero General Public License 3 or any later version.
+# See LICENSE.txt or <http://www.gnu.org/licenses/agpl.html>
+#
+""" Unit test cases for the `Permission` model."""
+
+import datetime
+import sqlalchemy as sa
+
+from archeologicaladdressbook import model
+from archeologicaladdressbook.model import Session
+from archeologicaladdressbook.tests.model import *
+from archeologicaladdressbook.tests.model.fixtures import *
+
+
+class TestPermissionModel(TestModel):
+    """ Unit test cases for the `Permission` model."""
+
+    def setUp(self):
+        """ Extend the method used to build a test database."""
+        super(TestPermissionModel, self).setUp()
+        #user_fixture()
+        permission_fixture()
+
+    def test_01_columns(self):
+        """ Test the `Permission` model columns and types."""
+        permission = Session.query(model.Permission).filter_by().first()
+        assert isinstance(permission.permission_id, int), '`permission_id` column is missing or has changed.'
+        assert isinstance(permission.permission_name, unicode), '`permission_name` column is missing or has changed.'
+        assert isinstance(permission.description, unicode), '`description` column is missing or has changed.'
+
+    def test_02_unique_constraint(self):
+        """ Test for unique constraint for the `Permission` model.
+
+        Test the unique constraint on `group_name`.
+        """
+        test_permission = DuplicatePermissionData.View()
+        permission = model.Permission(
+            permission_name = test_permission.permission_name,
+            description = test_permission.description
+        )
+        Session.add(permission)
+        try:
+            Session.commit()
+            raise AssertionError('`Permission` unique constraint on `group_name`, is missing.')
+        except sa.exc.IntegrityError:
+            Session.rollback()
---
+++ archeobases/branches/1.9.x/ArcheologicalAddressbook/archeologicaladdressbook/tests/model/test_user.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 LDLP (Laboratoire Départemental de Prehistoire du Lazaret)
+# http://lazaret.unice.fr/opensource/ - opensource@lazaret.unice.fr
+#
+# This file is part of ArcheologicalAdressbook and is released under
+# the GNU Affero General Public License 3 or any later version.
+# See LICENSE.txt or <http://www.gnu.org/licenses/agpl.html>
+#
+""" Unit test cases for the `User` model."""
+
+import datetime
+import sqlalchemy as sa
+
+from archeologicaladdressbook import model
+from archeologicaladdressbook.model import Session
+from archeologicaladdressbook.tests.model import *
+from archeologicaladdressbook.tests.model.fixtures import *
+
+
+class TestUserModel(TestModel):
+    """ Unit test cases for the `User` model."""
+
+    def setUp(self):
+        """ Extend the method used to build a test database."""
+        super(TestUserModel, self).setUp()
+        user_fixture()
+
+    def test_01_columns(self):
+        """ Test the `User` model columns and types."""
+        user = Session.query(model.User).filter_by().first()
+        assert isinstance(user.user_id, int), '`user_id` column is missing or has changed.'
+        assert isinstance(user.user_name, unicode), '`user_name` column is missing or has changed.'
+        assert isinstance(user.email_address, unicode), '`email_address` column is missing or has changed.'
+        assert isinstance(user.password, unicode), '`password` column is missing or has changed.'
+        assert isinstance(user.display_name, unicode), '`display_name` column is missing or has changed.'
+        assert isinstance(user.created, datetime.date), '`created` column is missing or has changed.'
+
+    def test_02_unique_constraint(self):
+        """ Test for unique constraint for the `User` model.
+
+        Test the unique constraint on `user_name`.
+        """
+        test_user = DuplicateUserData.Guest()
+        user = model.User(
+            user_name = test_user.user_name,
+            email_address = test_user.email_address,
+            password = test_user.password,
+            display_name = test_user.display_name
+        )
+        Session.add(user)
+        try:
+            Session.commit()
+            raise AssertionError('`User` unique constraint on `user_name`, is missing.')
+        except sa.exc.IntegrityError:
+            Session.rollback()

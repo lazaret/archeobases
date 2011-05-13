e017e11304ee740067aaab227116558f3863525c
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2011-05-13 14:55:42 +0000

    typo

--- archeobases/branches/1.9.x/bego/bego/model/meta.py
+++ archeobases/branches/1.9.x/bego/bego/model/meta.py
@@ -1,7 +1,7 @@
 """SQLAlchemy Metadata and Session object"""
 
-from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 __all__ = ['Session', 'engine', 'metadata', 'DeclarativeBase']

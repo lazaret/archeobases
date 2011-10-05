b75b73ac047c8d86852fda05f49c7be1336aa5ce
gabriel <gabriel@dee540c9-f427-0410-9603-694751ae893f>
2011-10-05 09:34:45 +0000

    global config for URL variables

---
+++ archeobases/branches/1.9.x/PetroglyphsMap/petroglyphsmap/public/config.js
@@ -0,0 +1,3 @@
+var weburl = 'http://127.0.0.1:80/'; // URL for the GIS
+var mappath = 'C:\\ms4w\\Apache\\htdocs\\'; // path to MapFiles
+var MSurl = 'http://127.0.0.1:80/cgi-bin/mapserv.exe?map='; // URL for MapServer

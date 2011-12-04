047ed3cec8d5f97dbb8c0f4ace1c1278b083a7a3
gabriel <gabriel@dee540c9-f427-0410-9603-694751ae893f>
2011-12-04 14:43:37 +0000

    hide connection infos

--- archeobases/branches/1.9.x/PetroglyphsMap/petroglyphsmap/public/config.php
+++ archeobases/branches/1.9.x/PetroglyphsMap/petroglyphsmap/public/config.php
@@ -6,7 +6,7 @@ session_start ();
 * Initialize SQL query
 */
 ini_set ('max_execution_time', 0); // no time limit
-$sessionpg = pg_connect("host=localhost port=5432 dbname=begogeo user=postgres password=postgres");
+$sessionpg = pg_connect("host=#########");
 
 /*
 * Variables

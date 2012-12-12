efb1d58c3363487855b1025c91c962da1e303e54
bertrand <bertrand@dee540c9-f427-0410-9603-694751ae893f>
2012-12-12 14:56:26 +0000

    Add pdfmap archive

---
+++ archeobases/trunk/genmap/pdfmap/pdfmap/version.py
@@ -0,0 +1,29 @@
+# PDFMap
+#
+# version  - (c) 2001-2004 Jerome Alet
+#            (c) 2001-2004 C@LL - Conseil Internet & Logiciels Libres
+#
+# (c) 2001-2004 Jerome Alet <alet@librelogiciel.com>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id: version.py,v 1.1 2006/01/10 15:31:14 jalet Exp $
+#
+
+__version__ = "3.00"
+
+__doc__ = """PDFMap : a powerful PDF map generation program."""
+
+__author__ = """Jerome Alet - alet@librelogiciel.com"""
+
